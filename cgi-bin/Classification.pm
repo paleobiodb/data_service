a29e0f8332139bbb887400b227d4f11655e66d20
John Alroy <john.alroy@mq.edu.au>
2003-07-03 00:10:01 +0000

    major rewrite and debugging

--- cgi-bin/Classification.pm
+++ cgi-bin/Classification.pm
@@ -19,10 +19,14 @@ sub get_classification_hash{
     my %full_class = ();
 
   foreach my $item (@taxon_names){
-	# Rank will start as genus
-	my $rank = "";
-	my $taxon_name = $item;
-	$full_class{$item} = "";
+# Rank will start as genus
+    my $rank = "";
+    my $taxon_name = $item;
+    my $base_taxon_name = $item;
+    my %family = ();
+    my %order = ();
+    my %class = ();
+    $full_class{$item} = "";
     my $child_no = -1;
     my $parent_no = -1;
     my %parent_no_visits = ();
@@ -42,6 +46,14 @@ sub get_classification_hash{
         if(defined $results[0]){
             # Save the taxon_no for keying into the opinions table.
             $child_no = $results[0]->{taxon_no};
+            my $base_rank = $results[0]->{taxon_rank};
+            if ( $base_rank =~ /family/ )	{
+              $family{$taxon_name} = $taxon_name;
+            } elsif ( $base_rank =~ /order/ )	{
+              $order{$taxon_name} = $taxon_name;
+            } elsif ( $base_rank =~ /class/ )	{
+              $class{$taxon_name} = $taxon_name;
+            }
 
             # Insurance for self referential / bad data in database.
             # NOTE: can't use the tertiary operator with hashes...
@@ -88,77 +100,32 @@ sub get_classification_hash{
                            "WHERE taxon_no=$parent_no";
                 @results = @{$dbt->getData($sql_auth)};
                 if(scalar @results){
-                    $auth_hash_ref = $results[0];
-                    # reset name and rank for next loop pass
-                    $rank = $auth_hash_ref->{"taxon_rank"};
-                    $taxon_name = $auth_hash_ref->{"taxon_name"};
-					# Quit if we're already at 'class' rank level or higher.
-					last if($results[0]->{taxon_rank} && $taxon_rank_order{$results[0]->{taxon_rank}} < $taxon_rank_order{$highest_level});
-					if($rank eq "family"){
-						unless(exists $family{$taxon_name}){
-							$family{$taxon_name} = $taxon_name;
-						}
-						$full_class{$item} = \$family{$taxon_name};
-						# Skip ahead if we've already got the hierarchy
-						# $order_ref will either be a scalar or a hash ref.
-						if(UNIVERSAL::isa($full_class{$item},"HASH")){
-							my ($family, $order_ref)=each %{$full_class{$item}};
-							# If the $order_ref is a hash, we've got the whole
-							# hierarchy.
-							if(UNIVERSAL::isa($order_ref,"HASH") && $full_class{$item}->{$family}->{${$order_ref}} ne ""){
-								last;	
-							}
-						}
-					}
-					elsif($rank eq "order"){
-						unless(exists $order{$taxon_name}){
-							$order{$taxon_name} = $taxon_name;
-						}
-						else{ 
-						}
-						# if we found an order, the family must already be known
-						my $family = $full_class{$item};
-						if(UNIVERSAL::isa($family,"SCALAR")){
-							$full_class{$item} = {};
-							$full_class{$item}->{${$family}} =\$order{$taxon_name};
-						}
-						elsif(UNIVERSAL::isa($family, "HASH")){
-							my ($fam_str, $order_ref)=each %{$full_class{$item}};
-                            # If the $order_ref is a hash, we've got the whole
-                            # hierarchy.
-                            if(UNIVERSAL::isa($order_ref,"HASH")){
-								my ($order_str, $class_ref)=each %{$order_ref};
-								if(UNIVERSAL::isa($class_ref, "HASH")){
-									last;   
-								}
-                            }
-						}
-					}
-					elsif($rank eq "class"){
-						unless(exists $class{$taxon_name}){
-							$class{$taxon_name} = $taxon_name;
-							#print "added CLASS $taxon_name\n";
-						}
-						else{ 
-							#print "CLASS $taxon_name already stashed\n";
-						}
-						# if we found a class, the family and order must 
-						# already be known
-						# $full_class{$item} is a hash lookup whose value is
-						# a hash reference from the %family hash.  Dereferencing
-						# that hash gives me the family key and the value,
-						# which should be a hash reference from the %order
-						# hash. If the %order reference's value is a scalar,
-						# we just have the order key=value, so we put in a ref
-						# to a %class hash (so we know if we've got an order
-						# scalar or a class scalar.
-						my ($family, $order_ref) = each %{$full_class{$item}};
-						if(UNIVERSAL::isa($order_ref,"SCALAR")){
-							my $order = $full_class{$item}->{$family};
-							$full_class{$item}->{$family} = {};
-							$full_class{$item}->{$family}->{${$order}} = \$class{$taxon_name};
-						}
-					}
+			$auth_hash_ref = $results[0];
+			# reset name and rank for next loop pass
+			$rank = $auth_hash_ref->{"taxon_rank"};
+			$taxon_name = $auth_hash_ref->{"taxon_name"};
+			# Quit if we're already at 'class' rank level or higher.
+			last if($results[0]->{taxon_rank} && $taxon_rank_order{$results[0]->{taxon_rank}} < $taxon_rank_order{$highest_level});
+			if($rank eq "family"){
+				$family{$base_taxon_name} = $taxon_name;
+				# Skip ahead if we've already got the hierarchy
+				# $order_ref will either be a scalar or a hash ref.
+				if ( $order{$taxon_name} )	{
+					last;
+				}
+			}
+			elsif($rank eq "order"){
+				$order{$base_taxon_name} = $taxon_name;
+				$order{$family{$base_taxon_name}} = $taxon_name;
+				if ( $class{$taxon_name} )	{
+					last;
+				}
+			}
+			elsif($rank eq "class"){
+				$class{$base_taxon_name} = $taxon_name;
+				$class{$family{$base_taxon_name}} = $taxon_name;
+				$class{$order{$base_taxon_name}} = $taxon_name;
+			}
                 }       
                 else{   
                     # No results might not be an error: 
@@ -177,7 +144,15 @@ sub get_classification_hash{
             # print "ERROR in sql: $sql_opin<br>";
             last;                 
         }       
-    }       
+    }
+# DOESN'T HELP
+  # if ( ! $order{$base_taxon_name} )	{
+  #   $order{$base_taxon_name} = $order{$family{$base_taxon_name}};
+  # }
+  # if ( ! $class{$base_taxon_name} )	{
+  #   $class{$base_taxon_name} = $class{$order{$base_taxon_name}};
+  # }
+    $full_class{$base_taxon_name} = $class{$base_taxon_name} .",". $order{$base_taxon_name} .",". $family{$base_taxon_name};
   }
   return \%full_class;
 }
