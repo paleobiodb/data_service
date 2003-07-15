1ea3ca49d5fb6911c4382878e291a429e27cf715
John Alroy <john.alroy@mq.edu.au>
2003-07-15 17:30:26 +0000

    added PACED values

--- cgi-bin/Map.pm
+++ cgi-bin/Map.pm
@@ -99,7 +99,7 @@ sub buildMapOnly {
 		# if a research project (not a group) is requested, get a list of
 		#  references included in that project JA 3.10.02
 		my $reflist;
-		if ( $q->param('research_group') =~ /(^ETE$)|(^5%$)|(^PGAP$)/ )	{
+		if ( $q->param('research_group') =~ /(^ETE$)|(^5%$)|(^PACED$)|(^PGAP$)/ )	{
 			$sql = "SELECT reference_no FROM refs WHERE project_name LIKE '%";
 			$sql .= $q->param('research_group') . "%'";
 
@@ -326,7 +326,7 @@ sub buildMapOnly {
 				}
 				$where = &::buildWhere ( $where, qq| modified>$filledfields{$t}| );
 			# following written by JA 3.10.02
-			} elsif ( $t eq "research_group" && $q->param('research_group') =~ /(^ETE$)|(^5%$)|(^PGAP$)/ )	{
+			} elsif ( $t eq "research_group" && $q->param('research_group') =~ /(^ETE$)|(^5%$)|(^PACED$)|(^PGAP$)/ )	{
 				$where .= " AND reference_no IN (" . $reflist . ")";
 			} elsif ( $t eq "research_group" ) {
 				# research_group is now a set -- tone 7 jun 2002
