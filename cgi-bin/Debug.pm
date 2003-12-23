ede7cd8c43149b2878db904c11d61c6e859e2485
Ryan Poling <poling@nowhere.edu>
2003-12-23 21:28:03 +0000

    used for logging errors

---
+++ cgi-bin/Debug.pm
@@ -0,0 +1,21 @@
+package Debug;
+
+
+# prints the passed string to a debug file
+# called "debug_log"
+# added by ryan on 12/18/2003
+sub dbPrint {
+	$| = 1;	# flushes buffer immediately
+
+	open LOG, ">>debug_log";
+	my $date = `date`;
+	chomp($date);
+ 
+	my $string = shift;
+	chomp($string);
+ 
+	print LOG "$date: $string \n";
+}
+
+1
+
