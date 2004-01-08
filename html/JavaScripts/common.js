518b2da1d11882bc4ed9c3a6ea47c9d4c669e571
Ryan Poling <poling@nowhere.edu>
2004-01-08 01:01:21 +0000

    used to be fillYears.js

---
+++ html/JavaScripts/common.js
@@ -0,0 +1,15 @@
+//meant to fill the years in a pull down menu from 1998 to the current year.
+
+function fillme() {
+	var yearField = document.forms[0].year
+	var date = new Date();
+
+	var maxYear = date.getFullYear(); 
+	var year;
+
+	for (year = maxYear; year >= 1998; year--) {
+		document.write("<option>" + year + "</option>");
+	}
+	
+	return 5;
+}
