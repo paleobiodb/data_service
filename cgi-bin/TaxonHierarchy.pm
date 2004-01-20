6c5ff52edf1c92efeb746f3b29e998a48d4f3c59
Ryan Poling <poling@nowhere.edu>
2004-01-20 19:23:21 +0000

    works

--- cgi-bin/TaxonHierarchy.pm
+++ cgi-bin/TaxonHierarchy.pm
@@ -59,8 +59,11 @@ sub getTaxonNameFromNumber {
 		$sql->setSQLExpr("SELECT taxon_name FROM authorities 
 				WHERE taxon_no = $input");
 
-		my @result = $sql->resultArrayForExecutingSQL();
+		$sql->executeSQL();
+		
+		my @result = $sql->nextResultRow();
 		my $tn = $result[0];
+		$sql->finishSQL();
 		
 		if ($tn) {
 			return $tn;
@@ -81,8 +84,11 @@ sub getTaxonNumberFromName {
 		$sql->setSQLExpr("SELECT taxon_no FROM authorities 
 				WHERE taxon_name = '$input'");
 
-		my @result = $sql->resultArrayForExecutingSQL();
+		$sql->executeSQL();
+		
+		my @result = $sql->nextResultRow();
 		my $tn = $result[0];
+		$sql->finishSQL();
 		
 		if ($tn) {
 			return $tn;
@@ -160,56 +166,147 @@ sub createTaxaHash {
 	# get the initial taxon the user set
 	my $tn = $self->taxonNumber();
 	my %hash;
+	my $ref_has_opinion;  # boolean
+	my ($pubyr, $idNum);
+	my (@result, @subResult);  # sql query results
+	
+	# another sql object for executing subqueries.
+	my $subSQL = SQLBuilder->new();
 	
 	# go up the hierarchy to the top (kingdom)
+	# from the rank the user started with.
 	while ($tn) {
-		$sql->setSQLExpr("SELECT parent_no FROM opinions WHERE child_no = $tn");
-		my @result = $sql->resultArrayForExecutingSQL();
-		my $parent = $result[0];
+		# note, the "ORDER BY o.parent_no ASC" is important - this means that if we have two
+		# rows with the same pubyr, then it will always fetch the last one added since 
+		# the numbers increment on each addition.
+		$sql->setSQLExpr("SELECT o.parent_no, o.pubyr, o.ref_has_opinion, 
+		o.reference_no, r.pubyr FROM opinions o, refs r 
+		WHERE o.child_no = $tn AND o.reference_no = r.reference_no ORDER BY o.parent_no ASC");
+		$sql->executeSQL();
 
+		# loop through all result rows, and find the one with the most
+		# recent pubyr.  Note, we'll have to look at the reference if the ref_has_opinion field is true.
+		$pubyr = 0;
+		$idNum = 0;
+		
+		my $tempYR;
+		while (@result = $sql->nextResultRow()) {			
+			if ($ref_has_opinion = $result[2]) {
+				# if ref_has_opinion is YES, then we need to look to the reference
+				# to find the pubyr
+				$tempYR = $result[4]; # pubyr from reference
+			} else {
+				$tempYR = $result[1];  # pubyr from opinion
+			}
+				
+			if ($tempYR > $pubyr) {
+				$pubyr = $tempYR;
+				$idNum = $result[0];
+			}
+			
+		} # end while @result.
+
+		$sql->finishSQL();
+		my $parent = $idNum;  # this is the parent with the most recent pubyr.
+
+		
+		
 		# get the rank of the parent
 		$sql->clear();
-		$sql->setSQLExpr("SELECT taxon_rank FROM
-		authorities WHERE taxon_no = '$parent'");
-						 	
-		@result = $sql->resultArrayForExecutingSQL();
+		$sql->setSQLExpr("SELECT taxon_rank FROM authorities WHERE taxon_no = $parent");
+		$sql->executeSQL();
+			
+		@result = $sql->nextResultRow();
 		my $pRank = $result[0];
+		$sql->finishSQL();
 		
 		# insert it into the hash, so we have the parent rank as the key
 		# and the parent number as the value.
 		$hash{$pRank} = $parent;
-				
+		
+		# also insert the pubyr for this parent, keyed to the id number.
+		$hash{$parent} = $pubyr;		
+					
 		$tn = $parent;
+		
+		#print "tn = $tn, id = $idNum, pubyr = $pubyr\n";
 	}
+	
 	
 	# now go down the hierarchy to the bottom
 	# starting from the taxon the user originally passed in
 	$tn = $self->taxonNumber();
 	while ($tn) {
-		$sql->setSQLExpr("SELECT child_no FROM opinions WHERE parent_no = $tn");
-		my @result = $sql->resultArrayForExecutingSQL();
-		my $child = $result[0];
+		$sql->setSQLExpr("SELECT o.child_no, o.pubyr, o.ref_has_opinion,
+		o.reference_no, r.pubyr FROM opinions o, refs r 
+		WHERE o.parent_no = $tn AND o.reference_no = r.reference_no ORDER BY o.child_no ASC");
+		$sql->executeSQL();
+
+		# loop through all result rows, and find the one with the most
+		# recent pubyr.  Note, we'll have to look at the reference if the ref_has_opinion field is true.
+		$pubyr = 0;
+		$idNum = 0;
+		
+		my $tempYR;
+		while (@result = $sql->nextResultRow()) {
+			if ($ref_has_opinion = $result[2]) {
+				# if ref_has_opinion is YES, then we need to look to the reference
+				# to find the pubyr
+				$tempYR = $result[4]; # pubyr from reference
+			} else {
+				$tempYR = $result[1];  # pubyr from opinion
+			}
+				
+			if ($tempYR > $pubyr) {
+				$pubyr = $tempYR;
+				$idNum = $result[0];
+			}
+			
+		} # end while @result.
+	
+	
+		$sql->finishSQL();
+		my $child = $idNum;  # this is the child with the most recent pubyr.
 
 		# get the rank of the parent
 		$sql->clear();
-		$sql->setSQLExpr("SELECT taxon_rank FROM
-		authorities WHERE taxon_no = '$child'");
-						 	
-		@result = $sql->resultArrayForExecutingSQL();
+		$sql->setSQLExpr("SELECT taxon_rank FROM authorities WHERE taxon_no = $child");
+		$sql->executeSQL();		
+			
+		@result = $sql->nextResultRow();
 		my $cRank = $result[0];
+		$sql->finishSQL();
+
 		
 		# insert it into the hash, so we have the parent rank as the key
 		# and the parent number as the value.
 		$hash{$cRank} = $child;
+		
+		# also insert the pubyr for this child, keyed to the id number.
+		$hash{$child} = $pubyr;
 				
-		$tn = $child;			
+		$tn = $child;
 	}
 	
+	
+	
 	my @keys = keys(%hash);
-	print "keys = @keys";
+	foreach my $key (@keys) {
+		$sql->clear();
+		
+		if (!($key =~ /\d/)) {
+			my $taxon_no = $hash{$key};
+		
+			$sql->setSQLExpr("SELECT taxon_name FROM authorities WHERE taxon_no = '$taxon_no'");
+			$sql->executeSQL();
+			my @result = $sql->nextResultRow();
+			my $taxon_name = $result[0];
+			print "$key = $taxon_name\n";
+		}
+	}
 		
 	
-	# store the hash in the object data field
+	#store the hash in the object data field
 	$self->{taxaHash} = %hash;
 	
 
