4fb466dcb56e118999ddab3218df93a3e5243b71
Ryan Poling <poling@nowhere.edu>
2004-02-05 23:54:03 +0000

    added function for cleaning strange characters from a string (including HTML)

---
+++ cgi-bin/Validation.pm
@@ -0,0 +1,81 @@
+# Written by rjp, 1/2004.
+# Used to validate data entered by a user, for example, in HTML forms.
+
+#
+# Each validate routine expects a to receive two parameters:
+# 1.  a string to verify
+# 2.  a maximum allowable length for that string
+# 
+# If no maximum length is passed, then it defaults to ANY length (ie, no max)
+#
+
+#
+# The clean routine cleans the passed string by removing all
+# questionable characters such as <,>*/|\~`", etc.
+
+package Validation;
+use strict;
+
+# pass this a string and it returns a "cleaned" copy of it.
+# basically removes any funny characters which shouldn't be in the database,
+# and also removes HTML code such as italics <i>
+sub clean {
+	my $in = shift;
+	
+	# remove html code
+	# looks for "<" followed by optional "/" followed by one or more
+	# characters which *aren't* "<" or ">" followed by ">".  This pretty
+	# much defines an HTML tag, so this should remove all of them.
+	$in =~ s/<[\/]?[^<>]+>//g;
+
+	# remove quote marks, etc..
+	$in =~ s/["'`]//g;
+	
+	return $in;
+}
+
+
+
+# this routine is meant for internal use only
+# pass it a string and a length, it returns true if the string is <= that length.
+sub lenCheck {
+	my $string = shift;
+	my $maxLen = shift;
+	if (($maxLen) && (length($string) > $maxLen)) {
+		return 0;
+	}
+	
+	return 1;
+}
+
+
+sub isNumeric {
+	my $string = shift;
+	my $maxLen = shift;
+	my $result;
+	
+	# starts with optional + or - followed by one or more digits
+	# followed by optional decimal point and more digits (also optional)
+	# OR
+	# starts with decimal point followed by one or more digits
+	$result = ($string =~ m/(^[-+]?\d+([.](\d+)?)?$)|(^[.]\d+$)/);
+	$result &= (lenCheck($string, $maxLen));
+	
+	return $result;
+}
+
+
+sub isInteger {
+	my $string = shift;
+	my $maxLen = shift;
+	my $result;
+	
+	$result = ($string =~ m/^\d+$/ );
+	$result &= (lenCheck($string, $maxLen));
+	
+	return $result;
+}
+
+
+
+1;
