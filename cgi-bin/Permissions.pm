d2364de7a1631f560d48f238191af86a92bb411e
Shanan Peters <peters@geology.wisc.edu>
2005-05-11 22:56:04 +0000

    Cleanout of old code

--- cgi-bin/Permissions.pm
+++ cgi-bin/Permissions.pm
@@ -15,66 +15,31 @@ package Permissions;
 
 use strict;
 
-use DBTransactionManager;
 use Debug;
 
-
-use constant CACHESIZE => 1000;		# number of entries in the cache.
-
-use fields qw(	
-				readCache
-				writeCache
-				readWriteCache
-				GLOBALVARS
-				session
-				DBTransactionManager
-				
-				date
-			);  # list of data members
+use fields qw(s date);
 
 # cache			:	reference to a cache hash of collection numbers and permissions.
 # date			:	stored for speed.. This object will never be alive more than a few seconds, so it's okay.
 # session		:	the session object passed in with new()
-# DBTransactionManager	:	DBTransactionManager object so we don't have to keep recreating it.
 			
 
 # Flags and constants
 # These are class variables.
 my $DEBUG = 0;		# DEBUG flag
 
-
 # **Note: Must pass the session variable when creating this object.
 sub new {
 	my $class = shift;
 	my Permissions $self = fields::new($class);
+
+	my $s = shift;		# session object
+	$self->{'s'} = $s;
 	
-	my $session = shift;		# session object
-	$self->{session} = $session;
-	
-	#hack for now.. we should pass this
-	my %GLOBALVARS;
-	$GLOBALVARS{session} = $session;
-	$self->{GLOBALVARS} = \%GLOBALVARS;
-	
-	if (! $session) {
-		Debug::logError("Permissions must be created with valid Session object!");
+	if (!$s) {
+		carp ("Permissions must be created with valid Session object!");
 		return undef;
 	}
-	
-	# create DBTransactionManager object
-	my $sql = DBTransactionManager->new();
-	# Note - be very careful - do it this way, otherwise we get an infinite loop.
-	$sql->setSession($session);
-	$self->{DBTransactionManager} = $sql;  # store in data member
-	
-	# create empty caches
-	my %emptyR = ();
-	my %emptyW = ();
-	my %emptyRW = ();
-	
-	$self->{readCache} = \%emptyR;
-	$self->{writeCache} = \%emptyW;
-	$self->{readWriteCache} = \%emptyRW;
 	
 	# store the date for speed.
 	# this is okay to do since the object won't be alive for more than a few seconds at a time.
@@ -84,127 +49,6 @@ sub new {
 }
 
 
-
-
-
-# pass it a collection_no
-# returns a boolean value 
-# false (0) if the user can't read this collection
-# true (1) if they can.
-#
-# Note, this may be slower than the older method, but it is cleaner
-# since it only requires the collection_no to determine access.
-#
-# Tested on XServe, and it took 6.2 seconds to check 10,000 rows = 0.00063 s. per row
-# which isn't too bad.
-#
-# by rjp, 1/2004.
-sub userHasReadPermissionForCollectionNumber {
-	my $self = shift;
-	
-	my $cnum = shift; 	# the collection_no to check
-	
-	if (! $cnum) {
-		Debug::logError("Permissions, no collection_no passed.");
-		return 0;  # false if no collection_no
-	}
-	
-	if (! $self->{session}) {
-		return 0;
-	}
-	
-	# grab a reference to the readCache.		
-	my $cacheRef = $self->{readCache};
-	
-	#my %cache = %$cacheRef;
-	#my @keys = keys(%cache);
-	#Debug::dbPrint("keys = @keys\n\n");
-	
-	
-	# first, check the cache to see if we already have a value for this collection no
-	if (my $cacheVal = $cacheRef->{$cnum}) {
-		return $cacheVal;	# if this entry in the cache hash exists,
-		# then we already know the permissions, and can just return it (0 or 1).
-	}
-	
-	
-	# if we make it to here, then the value was not cached,
-	# so we actually have to query the database.
-	
-	my $perm = $self->queryDatabaseForReadPerm($cnum);
-	
-	$cacheRef->{$cnum} = $perm;	# store the new value in the cache
-	$self->{readCache} = $cacheRef;
-	
-	return $perm;	# return it.	
-}
-
-
-
-# For INTERNAL use only.
-# actually queries the database to get read permissions for the passed
-# collection_no.  This is only used when the value is not *already* in the readCache. 
-sub queryDatabaseForReadPerm {
-	my $self = shift;
-	my $cnum = shift;
-
-	my $ses = $self->{session};
-	
-
-	my $sql = $self->{DBTransactionManager};
-	$sql->setSQLExpr("SELECT access_level, 
-						DATE_FORMAT(release_date,'%Y%m%d') rd_short,
-						research_group, authorizer 
-						FROM collections WHERE collection_no = $cnum");						
-	$sql->executeSQL();
-	my $resultRef = $sql->nextResultArrayRef();
-	
-	if (! $resultRef) {
-		return 0;
-	}
-	
-	my $access_level = $resultRef->[0];
-	my $rd_short = $resultRef->[1];
-
-	
-	
-	if ($rd_short < $self->{date}) {
-		# the release date has already passed, so it reverts to public access
-		return 1;		# okay to access.
-	}
-	
-	# if we make it to here, then the release date has not yet passed
-	my $session_auth = $ses->get("authorizer");
-	
-	my $authorizer = $resultRef->[3];	
-	
-	if ($access_level eq "the public") {
-		return 1;	# public access level is always visible, even if release date hasn't passed..	
-	}
-	
-	if (($ses->get("superuser") == 1) || ($session_auth eq $authorizer)) {
-		return 1;	# superuser can read anything
-					# and if the current authorizer authorized this record, then they can read it too.
-	}
-	
-	if (($access_level eq "database members") && ($session_auth !~ /guest/i) && ($session_auth ne "")){
-		return 1;	# okay as long as user isn't a guest
-	}
-	
-	if (($access_level eq "authorizer only") && ($session_auth eq $authorizer)) {
-		return 1;
-	}
-	
-	my $research_group = $resultRef->[2];
-	$research_group =~ tr/ /_/;		# replace spaces with underscores for comparison
-	
-	if (($access_level eq "group members") && ($ses->get($research_group))){
-		# note, spaces have already been replaced with underscores in the $research_group variable
-		return 1;
-	}
-	
-	return 0;		# if we make it to here, then there must be a problem, so disallow access.	
-}
 
 
 
@@ -220,7 +64,7 @@ sub getReadRows {
 	my $limit = shift;
 	my $ofRows = shift;
 	
-	my $s = $self->{session};
+	my $s = $self->{'s'};
 
 	# Get today's date in the lexical comparison format
 	my $now = $self->getDate ( );
@@ -333,7 +177,7 @@ sub getWriteRows {
 	my $limit = shift;
 	my $ofRows = shift;
 	
-	my $s = $self->{session};
+	my $s = $self->{'s'};
 
 	while ( my $row = $sth->fetchrow_hashref ( ) ) {
 
@@ -389,7 +233,7 @@ sub getReadWriteRowsForEdit{
 	my $self = shift;
 	my $sth = shift;
 
-	my $s = $self->{session};
+	my $s = $self->{'s'};
 	
 	# for returning data
 	my @results = ();
