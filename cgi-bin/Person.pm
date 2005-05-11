b2cf6a80e74ce996ecf42ad8a1b20e9294d4a768
Shanan Peters <peters@geology.wisc.edu>
2005-05-11 22:57:56 +0000

    Removal of object oriented stuff, cleanup of old code

--- cgi-bin/Person.pm
+++ cgi-bin/Person.pm
@@ -9,40 +9,6 @@ use strict;
 use DBI;
 use DBConnection;
 use DBTransactionManager;
-use URLMaker;
-use Constants;
-
-use fields qw(	
-				GLOBALVARS
-				
-				DBTransactionManager
-							);  # list of allowable data fields.
-
-						
-
-sub new {
-	my $class = shift;
-	my Person $self = fields::new($class);
-
-	$self->{GLOBALVARS} = shift;
-	
-	return $self;
-}
-
-
-# for internal use only!
-# returns the SQL builder object
-# or creates it if it has not yet been created
-sub getTransactionManager {
-	my Person $self = shift;
-	
-	my $DBTransactionManager = $self->{DBTransactionManager};
-	if (! $DBTransactionManager) {
-	    $DBTransactionManager = DBTransactionManager->new($self->{GLOBALVARS});
-	}
-	
-	return $DBTransactionManager;
-}
 
 
 # can pass it an optional argument, activeOnly
@@ -51,26 +17,14 @@ sub getTransactionManager {
 # Returns a matrix of authorizers with the 0th column
 # being the normal name and the 1st column the reversed name.
 sub listOfAuthorizers {
-	my Person $self = shift;
-	
+    my $dbt = shift;
 	my $activeOnly = shift;
 	
-	my $sql = $self->getTransactionManager();
-	
-	$sql->setSelectExpr("name, reversed_name FROM person");
-	$sql->setWhereSeparator("AND");
-	$sql->addWhereItem("is_authorizer = 1");
-	
-	if ($activeOnly) { 
-		$sql->addWhereItem("active = 1");
-	}
-	
-	$sql->setOrderByExpr("reversed_name");
-	
-	
-	my $res = $sql->allResultsArrayRef();
-	
-	return $res;	
+	my $sql = "SELECT name, reversed_name FROM person WHERE is_authorizer=1";
+	if ($activeOnly) { $sql .= " AND active = 1 "; }
+	$sql .= " ORDER BY reversed_name";
+
+    return $dbt->getData($sql);
 }
 
 
@@ -80,42 +34,51 @@ sub listOfAuthorizers {
 # Returns a matrix of enterers with the 0th column
 # being the normal name and the 1st column the reversed name.
 sub listOfEnterers {
-	my Person $self = shift;
-	
+    my $dbt = shift;
 	my $activeOnly = shift;
 	
-	my $sql = $self->getTransactionManager();
-	
-	my $temp = "SELECT name, reversed_name FROM person ";
-	if ($activeOnly) { $temp .= " WHERE active = 1 "; }
-	$temp .= " ORDER BY reversed_name";
-	
-	$sql->setSQLExpr($temp);
-	
-	my $res = $sql->allResultsArrayRef();
-	
-	return $res;
+	my $sql = "SELECT name, reversed_name FROM person ";
+	if ($activeOnly) { $sql .= " WHERE active = 1 "; }
+	$sql .= " ORDER BY reversed_name";
+
+    return $dbt->getData($sql);
 	
 }
 
 # Pass this an enterer or authorizer name (reversed or normal - doesn't matter)
 # Returns a true value if the name exists in our database of people.
-sub isValidName {
-	my Person $self = shift;
+sub checkName {
+    my $dbt = shift;
 	my $name = shift;
-	
-	my $sql = $self->getTransactionManager();
-	
-#	my $count = $sql->getSingleSQLResult("SELECT COUNT(*) FROM person WHERE
-#		name = '$name' OR reversed_name = '$name'");
-	my $count = ${$sql->getData("SELECT COUNT(*) as c FROM person WHERE
-		name = '$name' OR reversed_name = '$name'")}[0]->{c};	
 
-	if ($count) { return TRUE; }
-	else { return FALSE; }
+    my $sql = "SELECT COUNT(*) as c FROM person WHERE name=".$dbt->dbh->quote($name);
+	my $count = ${$dbt->getData($sql)}[0]->{'c'};	
+
+	if ($count) { return 1; }
+	else { return 0; }
 }
 
+# pass this a person number and it 
+# will return the person's name
+sub getPersonName {
+    my $dbt = shift;
+    my $num = shift;
 
+    if (! $num) {
+        return '';
+    }
+    my $result = ${$dbt->getData("SELECT name FROM person WHERE person_no=$num")}[0]->{'name'};
+
+    return $result;
+}
+
+# a trivial function - reverse the order of the last name and initial
+# If it was Sepkoski, J. before, now its J. Sepkoski
+sub reverseName {
+    my $name = shift;
+    $name =~ s/^\s*(.*)\s*,\s*(.*)\s*$/$2 $1/;
+    return $name;
+}
 
 
 # end of Person.pm
