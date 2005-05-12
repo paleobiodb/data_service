4fb8863b15e289695eaa05971725fd17a960ec40
Shanan Peters <peters@geology.wisc.edu>
2005-05-12 21:28:16 +0000

    changed doCompleteAuthorer,doCompleteEnter to generic function doComplete

--- html/JavaScripts/login.js
+++ html/JavaScripts/login.js
@@ -3,59 +3,38 @@
 // complete the authorizer field in the form.
 // must also pass it an array refernece to
 // a list of names in alphabetical order
-function doCompleteAuthorizer(e, names) {
+function doComplete(e, form_elem, names) {
 	
     //return if they press delete.
     if (e.which == 8 || e.which == 127) {
         return true;
     }
 
-    var val = document.forms[0].authorizer.value;
+    var val = form_elem.value;
     var sub = val.substr(0, val.length -1);
     
     //check to make sure that they haven't already typed in a valid
     //name..  If they have, then don't let 
     //them type in any extra characters
     if (alreadyPresent(sub, names)) {
-        document.forms[0].authorizer.value = sub;
+        form_elem.value = sub;
         return;
     }
     
-    //see if the partial name they've typed in matches.    
-    var match = doComplete(document.forms[0].authorizer.value, names);
+    // loop through each element in the names list
+    var match = new RegExp("^" + form_elem.value, "i");
     
-    if (match) {
-        document.forms[0].authorizer.value = match;
-    }
-}
-
-// complete the enterer field in the form.
-// must also pass it an array refernece to
-// a list of names in alphabetical order
-function doCompleteEnterer(e, names) {
-
-    //return if they press delete.
-    if (e.which == 8 || e.which == 127) {
-        return true;
-    }
-
-	    
-    var val = document.forms[0].enterer.value;
-    var sub = val.substr(0, val.length -1);
-    
-    //check to make sure that they haven't already typed in a valid
-    //name..  If they have, then don't let 
-    //them type in any extra characters
-    if (alreadyPresent(sub, names)) {
-        document.forms[0].enterer.value = sub;
-        return;
+    var lastMatch = "";
+    var numMatches = 0;
+    for (var i = 0; i < names.length; i++) {
+        if (match.test(names[i])) {
+            numMatches++;
+            lastMatch = names[i];
+        }
     }
     
-    //see if the partial name they've typed in matches.
-    var match = doComplete(val, names);
-    
-    if (match) {
-        document.forms[0].enterer.value = match;
+    if (numMatches == 1) {
+        form_elem.value =lastMatch;
     }
 }
 
@@ -76,35 +55,5 @@ function alreadyPresent(input, names) {
     return false;
 }
 
-
-// pass it a value to match on, and an array of possible matches
-function doComplete(input, names) {
-    if (input == "") {
-        return "";
-    }
-    
-    var len = input.length;
-    
-    var numNames = names.length;
-        
-    // loop through each element in the names list
-    var match = new RegExp("^" + input, "i");
-    var lastMatch = "";
-    
-    var numMatches = 0;
-    var i;
-    for (i = 0; i < numNames; i++) {
-        if (match.test(names[i])) {
-            numMatches++;
-            lastMatch = names[i];
-        }
-    }
-    
-    if (numMatches == 1) {
-        return lastMatch;
-    }
-    
-    return ""; //no match found            
-}
 
 
