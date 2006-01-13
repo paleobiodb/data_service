c1f1bb2d7c1c83ff5a4bab3d2bfda9f394ec5cb3
Shanan Peters <peters@geology.wisc.edu>
2006-01-13 23:31:14 +0000

    Resolves ip addresses in the request_log, if it can, and writes the resolved lines out to a file called request_log_resolved

---
+++ scripts/requestLogLookup.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+use Class::Date qw(date localdate gmdate now);
+
+use strict;
+
+open RLI, "</var/log/httpd/request_log"
+    or die "Could not open request_log";
+open RLO, ">>/var/log/httpd/request_log_resolved"
+    or die "Could not open request_log_resolved";
+
+
+my $last_line_resolved = `tail -1 /var/log/httpd/request_log_resolved`;
+my ($ip,$date,@rest) = split(/\t/,$last_line_resolved);
+my $time = 0;
+my $lastDate;
+if ($date) {
+#    print "Last date resolved: $date\n";
+    $lastDate = new Class::Date $date;
+} else {
+    $lastDate = new Class::Date "2000-01-01";
+}
+
+
+my $skipCount = 0;
+my $resolveCount = 0;
+while (my $line = <RLI>) {
+    my ($ip,$date,@rest) = split(/\t/,$line);
+
+    my $lineDate = new Class::Date $date;
+
+    if ($lineDate >= $lastDate) {
+        $resolveCount++;
+        my $text = `host $ip`;
+        chomp($text);
+        my $host;
+        if ($text =~ /domain name pointer (.*?)$/) {
+            $host = $1;
+            $host =~ s/\.$//;
+        } else {
+            $host = $ip;
+#            print "Can't lookup $ip: $text\n";
+        }
+        print RLO "$host\t$date\t".join("\t",@rest);
+    } else {
+        $skipCount++;
+    }
+}
+
+#print "Skipped $skipCount and resolved $resolveCount\n";
