539028e10526c144e7441fbd1bbcdb46d2cbb9bd
Shanan Peters <peters@geology.wisc.edu>
2006-01-13 19:39:00 +0000

    autocomplete works a bit nice for time interval terms

--- html/JavaScripts/login.js
+++ html/JavaScripts/login.js
@@ -3,7 +3,7 @@
 // complete the authorizer field in the form.
 // must also pass it an array refernece to
 // a list of names in alphabetical order
-function doComplete(e, form_elem, names, skip_already_present_check) {
+function doComplete(e, form_elem, names, skip_check_if_space) {
 	
     //return if they press delete.
     if (e.which == 8 || e.which == 127) {
@@ -16,7 +16,10 @@ function doComplete(e, form_elem, names, skip_already_present_check) {
     //check to make sure that they haven't already typed in a valid
     //name..  If they have, then don't let 
     //them type in any extra characters
-    if (!skip_already_present_check) {
+    var lastChar = val.substr(val.length-1,1);
+    // Only skip the check if the last char is a space (only relevant so ppl can enter
+    // 10 my bin terms in interval form
+    if (!(lastChar == " " && skip_check_if_space)) {
         if (alreadyPresent(sub, names)) {
             form_elem.value = sub;
             return;
