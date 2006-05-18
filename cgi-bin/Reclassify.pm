b1ef37f5bc15d3775f21637e4c648a03ea2820ef
Shanan Peters <peters@geology.wisc.edu>
2006-05-18 01:58:33 +0000

    Changes to accomodate fuzzier matches between occurrences and taxon table.  Accomplished by calling Taxon::getBestClassification, which can return an array of all possible classification choices.  Changes to accomodate subgenera

--- cgi-bin/Reclassify.pm
+++ cgi-bin/Reclassify.pm
@@ -53,14 +53,19 @@ sub displayOccurrenceReclassify	{
     my @occrefs;
     if (@collections) {
 	    print "<center><h3>Classification of ".$q->param('taxon_name')."</h3>";
-        my ($genus,$species) = split(/\s+/,$q->param('taxon_name'));
-        my $sql = "(SELECT 0 reid_no, o.authorizer_no, o.occurrence_no,o.taxon_no, o.genus_reso, o.genus_name, o.species_reso, o.species_name, c.collection_no, c.collection_name, c.country, c.state, c.max_interval_no, c.min_interval_no FROM occurrences o, collections c WHERE o.collection_no=c.collection_no AND c.collection_no IN (".join(", ",@collections).") AND o.genus_name LIKE ".$dbh->quote($genus);
+        my ($genus,$subgenus,$species,$subspecies) = Taxon::splitTaxon($q->param('taxon_name'));
+        my @names = ($dbh->quote($genus));
+        if ($subgenus) {
+            push @names, $dbh->quote($subgenus);
+        }
+        my $names = join(", ",@names);
+        my $sql = "(SELECT 0 reid_no, o.authorizer_no, o.occurrence_no,o.taxon_no, o.genus_reso, o.genus_name, o.subgenus_reso, o.subgenus_name, o.species_reso, o.species_name, c.collection_no, c.collection_name, c.country, c.state, c.max_interval_no, c.min_interval_no FROM occurrences o, collections c WHERE o.collection_no=c.collection_no AND c.collection_no IN (".join(", ",@collections).") AND (o.genus_name IN ($names) OR o.subgenus_name IN ($names))";
         if ($species) {
             $sql .= " AND o.species_name LIKE ".$dbh->quote($species);
         }
         $sql .= ")";
         $sql .= " UNION ";
-        $sql .= "( SELECT re.reid_no, re.authorizer_no,re.occurrence_no,re.taxon_no, re.genus_reso, re.genus_name, re.species_reso, re.species_name, c.collection_no, c.collection_name, c.country, c.state, c.max_interval_no, c.min_interval_no FROM reidentifications re, occurrences o, collections c WHERE re.occurrence_no=o.occurrence_no AND o.collection_no=c.collection_no AND c.collection_no IN (".join(", ",@collections).") AND re.genus_name LIKE ".$dbh->quote($genus);
+        $sql .= "( SELECT re.reid_no, re.authorizer_no,re.occurrence_no,re.taxon_no, re.genus_reso, re.genus_name, re.subgenus_reso, re.subgenus_name, re.species_reso, re.species_name, c.collection_no, c.collection_name, c.country, c.state, c.max_interval_no, c.min_interval_no FROM reidentifications re, occurrences o, collections c WHERE re.occurrence_no=o.occurrence_no AND o.collection_no=c.collection_no AND c.collection_no IN (".join(", ",@collections).") AND (re.genus_name IN ($names) OR re.subgenus_name IN ($names))";
         if ($species) {
             $sql .= " AND re.species_name LIKE ".$dbh->quote($species);
         }
@@ -74,9 +79,9 @@ sub displayOccurrenceReclassify	{
 
         # get all the occurrences
         my $collection_no = int($q->param('collection_no'));
-        $sql = "(SELECT 0 reid_no,authorizer_no, occurrence_no,taxon_no,genus_reso,genus_name,species_reso,species_name FROM occurrences WHERE collection_no=$collection_no)".
+        $sql = "(SELECT 0 reid_no,authorizer_no, occurrence_no,taxon_no,genus_reso,genus_name,subgenus_reso,subgenus_name,species_reso,species_name FROM occurrences WHERE collection_no=$collection_no)".
                " UNION ".
-               "(SELECT reid_no,authorizer_no, occurrence_no,taxon_no,genus_reso,genus_name,species_reso,species_name FROM reidentifications WHERE collection_no=$collection_no)".
+               "(SELECT reid_no,authorizer_no, occurrence_no,taxon_no,genus_reso,genus_name,subgenus_reso,subgenus_name,species_reso,species_name FROM reidentifications WHERE collection_no=$collection_no)".
                " ORDER BY occurrence_no ASC,reid_no ASC";
         main::dbg("Reclassify sql:".$sql);
         @occrefs = @{$dbt->getData($sql)};
@@ -85,15 +90,17 @@ sub displayOccurrenceReclassify	{
 	# tick through the occurrences
 	# NOTE: the list will be in data entry order, nothing fancy here
 	if ( @occrefs )	{
-		print "<hr>\n";
 		print "<form method=\"post\">\n";
 		print "<input id=\"action\" type=\"hidden\" name=\"action\" value=\"startProcessReclassifyForm\">\n";
         if (@collections) {
             print "<input type=\"hidden\" name=\"taxon_name\" value=\"".$q->param('taxon_name')."\">";
+		    print "<table border=0 cellpadding=0 cellspacing=0>\n";
+            print "<tr><th colspan=2>Collection</th><th>Classificaton based on</th></tr>";
         } else {
             print "<input type=\"hidden\" name=\"collection_no\" value=\"".$q->param('collection_no')."\">";
+		    print "<table border=0 cellpadding=0 cellspacing=0>\n";
+            print "<tr><th>Taxon name</th><th>Classification based on</th></tr>";
         }
-		print "<table border=0 cellpadding=0 cellspacing=0>\n";
 	}
 
     # Make non-editable links not changeable
@@ -103,7 +110,7 @@ sub displayOccurrenceReclassify	{
 	my $rowcolor = 0;
     my $nonEditableCount = 0;
     my @badoccrefs;
-    my $genusOnly = 0;
+    my $nonExact = 0;
 	for my $o ( @occrefs )	{
         my $editable = ($s->get("superuser") || $is_modifier_for{$o->{'authorizer_no'}} || $o->{'authorizer_no'} == $s->get('authorizer_no')) ? 1 : 0;
         my $disabled = ($editable) ?  '' : 'DISABLED';
@@ -119,26 +126,13 @@ sub displayOccurrenceReclassify	{
 		else	{
 			# compose the taxon name
 			my $taxon_name = $o->{genus_name};
-			if ( $o->{species_reso} !~ /informal/ && $o->{species_name} ne "sp." && $o->{species_name} ne "indet." )	{
+			if ( $o->{species_reso} !~ /informal/ && $o->{species_name} !~ /^sp\./ && $o->{species_name} !~ /^indet\./)	{
 				$taxon_name .= " " . $o->{species_name};
 			}
-
-			# find taxon names that match
-			my $sql = "SELECT taxon_no,ref_is_authority,authorities.author1last as aa1,authorities.author2last as aa2,authorities.otherauthors as aoa,authorities.pubyr as ayr,refs.author1last as ra1,refs.author2last as ra2,refs.otherauthors as roa,refs.pubyr as ryr FROM authorities,refs WHERE refs.reference_no=authorities.reference_no AND taxon_name='$taxon_name'";
-			my @taxnorefs = @{$dbt->getData($sql)};
-
-			# didn't work? maybe the genus is known even though
-			#  the species isn't
-			my $usedGenus = "";
-			if ( ! @taxnorefs && $taxon_name =~ / / )	{
-				($taxon_name) = split / /,$taxon_name;
-				$sql = "SELECT taxon_no,ref_is_authority,authorities.author1last as aa1,authorities.author2last as aa2,authorities.otherauthors as aoa,authorities.pubyr as ayr,refs.author1last as ra1,refs.author2last as ra2,refs.otherauthors as roa,refs.pubyr as ryr FROM authorities,refs WHERE refs.reference_no=authorities.reference_no AND taxon_name='$taxon_name'";
-				@taxnorefs = @{$dbt->getData($sql)};
-				$usedGenus = "YES";
-			}
+            @all_matches = Taxon::getBestClassification($dbt,$o->{'genus_reso'},$o->{'genus_name'},$o->{'subgenus_reso'},$o->{'subgenus_name'},$o->{'species_reso'},$o->{'species_name'});
 
 			# now print the name and the pulldown of authorities
-			if ( @taxnorefs )	{
+			if ( @all_matches )	{
 				if ( $rowcolor % 2 )	{
 					print "<tr>";
 				} else	{
@@ -173,11 +167,15 @@ sub displayOccurrenceReclassify	{
 
 				# here's the name
 				my $formatted = "";
-				if ( $o->{species_name} ne "indet." )	{
+				if ( $o->{'species_name'} !~ /^indet\./ )	{
 					$formatted .= "<i>";
 				}
-				$formatted .= "$o->{genus_reso} $o->{genus_name} $o->{species_reso} $o->{species_name}";
-				if ( $o->{species_name} ne "indet." )	{
+				$formatted .= "$o->{genus_reso} $o->{genus_name}";
+                if ($o->{'subgenus_name'}) {
+                    $formatted .= " $o->{subgenus_reso} ($o->{subgenus_name})";
+                }
+                $formatted .= " $o->{species_reso} $o->{species_name}";
+				if ( $o->{'species_name'} !~ /^indet\./ )	{
 					$formatted .= "</i>";
 				}
 
@@ -207,50 +205,33 @@ sub displayOccurrenceReclassify	{
 					print "<td>&nbsp;&nbsp;\n<select $disabled name='reid_taxon_no'>\n";
 				}
 				# populate the select list of authorities
-				for my $t ( @taxnorefs )	{
+				foreach my $m ( @all_matches)	{
+                    my $t = TaxonInfo::getTaxa($dbt,{'taxon_no'=>$m->{'taxon_no'}},['taxon_no','taxon_name','taxon_rank','author1last','author2last','otherauthors','pubyr']);
 					# have to format the authority data
-					my $authority = "";
-					# first try getting ref data from the
-					#  the reference for the record
-					if ( $t->{ref_is_authority} eq "YES" )	{ 
-						$authority .= $t->{ra1};
-						if ( $t->{roa} )	{
-							$authority .= " et al.";
-						} elsif ( $t->{ra2} )	{
-							$authority .= " and " . $t->{ra2};
-						}
-						$authority .= " " . $t->{ryr};
-					}
-					# failing that, use the directly
-					#  recorded values
-					elsif ( $t->{aa1} )	{
-						$authority .= $t->{aa1};
-						if ( $t->{aoa} )	{
-							$authority .= " et al.";
-						} elsif ( $t->{aa2} )	{
-							$authority .= " and " . $t->{aa2};
-						}
-						$authority .= " " . $t->{ayr};
-					}
-
+					my $authority = "$t->{taxon_name}";
+                    my $pub_info = Reference::formatShortRef($t);
+                    if ($pub_info =~ /[A-Za-z0-9]/) {
+                        $authority .= ", $pub_info";
+                    }
                 	# needed by Classification
                     my %master_class=%{TaxaCache::getParents($dbt, [$t->{'taxon_no'}],'array_full')};
 
 					my @parents = @{$master_class{$t->{'taxon_no'}}};
                     if (@parents) {
-						$authority .= " (";
+						$authority .= " [";
                         my $foundParent = 0;
                         foreach (@parents) {
                             if ($_->{'taxon_rank'} =~ /^(?:family|order|class)$/) {
                                 $foundParent = 1;
                                 $authority .= $_->{'taxon_name'}.", ";
+                                last;
                             }
                         }
                         $authority =~ s/, $//;
                         if (!$foundParent) {
                             $authority .= $parents[0]->{'taxon_name'};
                         }
-                        $authority .= ")";
+                        $authority .= "]";
                     }
 					if ( $authority !~ /[A-Za-z]/ )	{
 						$authority = "taxon number " . $t->{taxon_no};
@@ -259,9 +240,8 @@ sub displayOccurrenceReclassify	{
 					#  classification but no author
 					$authority =~ s/^ //;
 
-					if ( $usedGenus )	{
-						$authority = "genus: ".$authority;
-						$genusOnly++;
+					if ($m->{'match_level'} < 30)	{
+						$nonExact++;
 					}
 
 					print "<option value='" , $t->{taxon_no} , "+" , $authority , "'";
@@ -290,11 +270,11 @@ sub displayOccurrenceReclassify	{
 		print "</form>\n";
 	}
 	print "<p>\n";
-	if ( $genusOnly )	{
-		print "<p><i>\"genus\" means that only the genus could be classified</i></p>\n";
+	if ( $nonExact)	{
+		print "<p><div class=\"warning\">Exact formal classifications for some taxa could not be found, so approximate matches were used.  For example, a species might not be formally classified but its genus is.</div></p>\n";
 	}
     if ( $nonEditableCount) {
-        print "<p><i>Some occurrences can't be reclassified because they have a different authorizer</i></p>\n";
+        print "<p><div class=\"warning\">Some occurrences can't be reclassified because they have a different authorizer</div></p>\n";
     }
 
 	# print the informal and otherwise unclassifiable names
@@ -312,11 +292,15 @@ sub displayOccurrenceReclassify	{
 			print "<tr class='darkList'>";
 		}
 		print "<td align='left'>&nbsp;&nbsp;";
-		if ( $b->{species_name} ne "indet." )	{
+		if ( $b->{'species_name'} !~ /^indet\./)	{
 			print "<i>";
 		}
-		print "$b->{genus_reso} $b->{genus_name} $b->{species_reso} $b->{species_name}\n";
-		if ( $b->{species_name} ne "indet." )	{
+		print "$b->{genus_reso} $b->{genus_name}";
+        if ($b->{'subgenus_name'}) {
+            print " $b->{subgenus_reso} ($b->{subgenus_name})";
+        }
+        print " $b->{species_reso} $b->{species_name}\n";
+		if ( $b->{'species_name'} !~ /^indet\./)	{
 			print "</i>";
 		}
 		print "&nbsp;&nbsp;</td></tr>\n";
@@ -350,8 +334,12 @@ sub processReclassifyForm	{
         my $sql = "SELECT collection_name FROM collections WHERE collection_no=" . $q->param('collection_no');
         my $coll_name = ${$dbt->getData($sql)}[0]->{collection_name};
         print "<h3>Taxa reclassified in collection " , $q->param('collection_no') ," (" , $coll_name , ")</h3>\n\n";
+	    print "<table border=0 cellpadding=2 cellspacing=0>\n";
+        print "<tr><th>Taxon</th><th>Classification based on</th></tr>";
     } else {
         print "<h3>Taxa reclassified for " , $q->param('taxon_name') ,"</h3>\n\n";
+	    print "<table border=0 cellpadding=2 cellspacing=0>\n";
+        print "<tr><th colspan=2>Collection</th><th>Classification based on</th></tr>";
     }
 
 	# get lists of old and new taxon numbers
@@ -366,15 +354,16 @@ sub processReclassifyForm	{
 	my @new_reid_taxa = $q->param('reid_taxon_no');
 	my @reids = $q->param('reid_no');
 
-	print "<table border=0 cellpadding=2 cellspacing=0>\n";
 	my $rowcolor = 0;
 
 	# first tick through the occurrence taxa and update as appropriate
+    my $seen_reclassification = 0;
 	foreach my $i (0..$#old_taxa)	{
 		my $old_taxon_no = $old_taxa[$i];
         my $occurrence_description = uri_unescape($occurrence_descriptions[$i]);
 		my ($new_taxon_no,$authority) = split /\+/,$new_taxa[$i];
 		if ( $old_taxa[$i] != $new_taxa[$i] )	{
+            $seen_reclassification++;
 
 		# update the occurrences table
 			my $sql = "UPDATE occurrences SET taxon_no=".$new_taxon_no.
@@ -394,7 +383,7 @@ sub processReclassifyForm	{
 			} else	{
 				print "<tr class='darkList'>";
 			}
-			print "<td>&nbsp;&nbsp;$occurrence_description $authority&nbsp;&nbsp;</td>\n";
+			print "<td>&nbsp;&nbsp;$occurrence_description</td><td style=\"padding-left: 1em;\"> $authority&nbsp;&nbsp;</td>\n";
 			print "</tr>\n";
 			$rowcolor++;
 		}
@@ -408,6 +397,7 @@ sub processReclassifyForm	{
         my $reid_description = uri_unescape($reid_descriptions[$i]);
 		my ($new_taxon_no,$authority) = split /\+/,$new_reid_taxa[$i];
 		if ( $old_reid_taxa[$i] != $new_reid_taxa[$i] )	{
+            $seen_reclassification++;
 
 		# update the reidentifications table
 			my $sql = "UPDATE reidentifications SET taxon_no=".$new_taxon_no.
@@ -427,13 +417,16 @@ sub processReclassifyForm	{
 			} else	{
 				print "<tr class='darkList'>";
 			}
-			print "<td>&nbsp;&nbsp;$reid_description $authority&nbsp;&nbsp;</td>\n";
+			print "<td>&nbsp;&nbsp;$reid_description</td><td style=\"padding-left: 1em;\"> $authority&nbsp;&nbsp;</td>\n";
 			print "</tr>\n";
 			$rowcolor++;
 		}
 	}
 
 	print "</table>\n\n";
+    if (!$seen_reclassification) {
+        print "<div align=\"center\">No taxa reclassified</div>";
+    }
 
 	print "<p><a href=\"$exec_url?action=startStartReclassifyOccurrences&collection_no=";
 	print $q->param('collection_no');
