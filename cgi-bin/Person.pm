cc85ca377349c859b9481c5f6d686da875d47537
Shanan Peters <peters@geology.wisc.edu>
2006-06-12 22:31:15 +0000

    Added functions to display authorizers,enterers,institutions.  Added scramble function to scramble email addresses.  Javascript descramble function descrambles and prints out the email addresses.  Idea is that screen scraper type programs won't execute the javascript.  Changes so last_name,first_name are used instead of reversed_name.  Note that the name field still exsists since it doesn't always match up with the lat name and first name.  I.E Kevin Boyce is really 'C. Boyce' since Kevin is his middle name, some of the german guys have umlauts in their names, etc.

--- cgi-bin/Person.pm
+++ cgi-bin/Person.pm
@@ -1,44 +1,30 @@
-#!/usr/bin/perl
-
-# 3/2004 by rjp
-# represents information about the Person table in the database.
-
 package Person;
-
 use strict;
-use DBI;
-use DBTransactionManager;
-
 
 # can pass it an optional argument, activeOnly
 # if true, then only return active authorizers.
-#
-# Returns a matrix of authorizers with the 0th column
-# being the normal name and the 1st column the reversed name.
 sub listOfAuthorizers {
     my $dbt = shift;
 	my $activeOnly = shift;
 	
-	my $sql = "SELECT name, reversed_name, person_no FROM person WHERE is_authorizer=1";
+	my $sql = "SELECT name, person_no FROM person WHERE is_authorizer=1";
 	if ($activeOnly) { $sql .= " AND active = 1 "; }
-	$sql .= " ORDER BY reversed_name";
+	$sql .= " ORDER BY last_name,first_name";
 
     return $dbt->getData($sql);
 }
 
 
 # can pass it an optional argument, activeOnly
-# if true, then only return active enterers.
-#
-# Returns a matrix of enterers with the 0th column
-# being the normal name and the 1st column the reversed name.
+# if true, then only return active enterers. all authorizers
+# are counted as enterers as well
 sub listOfEnterers {
     my $dbt = shift;
 	my $activeOnly = shift;
 	
-	my $sql = "SELECT name, reversed_name, person_no FROM person ";
+	my $sql = "SELECT name, person_no FROM person ";
 	if ($activeOnly) { $sql .= " WHERE active = 1 "; }
-	$sql .= " ORDER BY reversed_name";
+	$sql .= " ORDER BY last_name,first_name";
 
     return $dbt->getData($sql);
 	
@@ -73,10 +59,126 @@ sub getPersonName {
 
 # a trivial function - reverse the order of the last name and initial
 # If it was Sepkoski, J. before, now its J. Sepkoski
+# Likewise J. Sepkoski will be reversed into Sepkoski, J.
 sub reverseName {
     my $name = shift;
-    $name =~ s/^\s*(.*)\s*,\s*(.*)\s*$/$2 $1/;
+    if ($name =~ /,/) {
+        $name =~ s/^\s*(.*)\s*,\s*(.*)\s*$/$2 $1/;
+    } else {
+        $name =~ s/^\s*(\w\.)\s*(.*)\s*$/$2, $1/;
+    }
     return $name;
+}
+
+sub displayEnterers {
+    my $dbt = shift;
+    my $html = "<div align=\"center\"><h3>List of data enterers</h3></div>";
+    $html .= "The following students and research assistants have entered data into the Database.
+    Institution names are placed in parentheses to indicate students who have since moved on.
+    <i>IMPORTANT: if your e-mail address is not on this list and should be, please notify <a href=\"mailto:alroy\@nceas.ucsb.edu\">John Alroy.</a></i><br><br>";
+
+    my $sql = "SELECT first_name,last_name,institution,email FROM person WHERE is_authorizer=0 ORDER BY last_name,first_name";
+    my @results = @{$dbt->getData($sql)};
+
+    $html .= '<table cellpadding="3" cellspacing="0" border="0" width="100%">';
+    $html .= "<tr><th align=\"left\">Name</th><th align=\"left\">Institution</th><th align=\"left\">Email</th></tr>";
+    for(my $i=0;$i<@results;$i++) {
+        my $row = $results[$i];
+        my $name = "$row->{first_name} $row->{last_name}";
+        if ($i % 2 == 0) {
+            $html .= "<tr class=\"darkList\">";
+        } else {
+            $html .= "<tr>";
+        }
+        my $email;
+        if ($row->{'email'}) {
+            $email = scramble("$row->{'email'}");
+        } else {
+            $email = "<i>address unknown</i>";
+        }
+        $html .= "<td>$name</td><td>$row->{institution}</td><td>$email</td></tr>";
+    }
+    $html .= "</table>";
+    return $html;
+}
+
+sub displayAuthorizers {
+    my $dbt = shift;
+    my $html = "";
+
+    my $sql = "SELECT first_name,last_name,institution,email FROM person WHERE is_authorizer=1 ORDER BY last_name,first_name";
+    my @results = @{$dbt->getData($sql)};
+
+    $html .= '<div align="center"><h2>List of participants</h2></div>';
+    $html .= '<div align="center"><h4>See also our list of <a href="/cgi-bin/bridge.pl?action=displayInstitutions">participating institutions</a></h4></div>';
+
+    $html .= '<table cellpadding="3" cellspacing="0" border="0" width="100%">';
+    $html .= "<tr><th align=\"left\">Name</th><th align=\"left\">Institution</th><th align=\"left\">Email</th></tr>";
+    for(my $i=0;$i<@results;$i++) {
+        my $row = $results[$i];
+        my $name = "$row->{first_name} $row->{last_name}";
+        if ($i % 2 == 0) {
+            $html .= "<tr class=\"darkList\">";
+        } else {
+            $html .= "<tr>";
+        }
+        my $email;
+        if ($row->{'email'}) {
+            $email = scramble("$row->{'email'}");
+        } else {
+            $email = "<i>address unknown</i>";
+        }
+        $html .= "<td>$name</td><td>$row->{institution}</td><td>$email</td></tr>";
+    }
+    $html .= "</table>";
+    return $html;
+}
+
+sub displayInstitutions {
+    my $dbt = shift;
+    my $html = ""; 
+
+    my $sql = "SELECT first_name,last_name,institution,email FROM person WHERE is_authorizer=1 ORDER BY last_name,first_name";
+    my @results = @{$dbt->getData($sql)};
+
+    my %institutions;
+    foreach my $row (@results) {
+        push @{$institutions{$row->{'institution'}}}, $row;
+    }
+    my @all_institutions = sort {$a cmp $b} keys %institutions;
+
+    $html .= '<div align="center"><h2>List of participating institutions</h2></div>';
+    $html .= '<div align="center"><h4>See also our list of <a href="/cgi-bin/bridge.pl?action=displayAuthorizers">participants</a></h4></div>';
+
+    $html .= '<table cellpadding="3" cellspacing="0" border="0" width="100%">';
+    $html .= "<tr><th align=\"left\">Institution</th><th align=\"left\">Database Members</th></tr>";
+    for(my $i=0;$i<@all_institutions;$i++) {
+        my $institution = $all_institutions[$i];
+        my @all_people = @{$institutions{$institution}};
+        my @names = map {$_->{'first_name'}." ".$_->{'last_name'}} @all_people;
+
+        if ($i % 2 == 0) {
+            $html .= "<tr class=\"darkList\">";
+        } else {
+            $html .= "<tr>";
+        }
+        $html .= "<td style=\"white-space: nowrap;\">$institution</td><td>".join(", ",@names)."</td></tr>";
+    }
+    $html .= "</table>";
+    return $html;
+}
+
+sub scramble {
+    my $email = shift;
+    if ($email =~ /@/) {
+        my ($part1,$part2) = split(/@/,$email);
+        $part2 = reverse($part2);
+        $part1 = reverse($part1);
+        $part1 =~ s/'/\\'/g;
+        $part2 =~ s/'/\\'/g;
+        $email = "<script type=\"text/javascript\">descram('$part2','$part1')</script>";
+    }  
+    return $email;
 }
 
 
