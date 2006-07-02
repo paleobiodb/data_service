2117b00678665b762b22cd42fa60cd747c1987c8
Shanan Peters <peters@geology.wisc.edu>
2006-07-02 09:01:58 +0000

    Added making of 'latest dump' for mirror purposes

--- system/mac/backup_scripts/mysqldump-pbdb.sh
+++ system/mac/backup_scripts/mysqldump-pbdb.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-MYSQL_BACKUP_DIR=/Volumes/pbdb_RAID/dailybackups
+MYSQL_BACKUP_DIR=/Volumes/pbdb_RAID/dailybackups/db
 OF=mysql-backup-$(date +%Y%m%d)
+LATEST=/Volumes/pbdb_RAID/dailybackups/pbdb_latest.gz
 
 # mysqldump method (preferred)
 mysqldump -u paleodbbackup -p'iIi4m2bd' --opt -B pbdb | gzip > $MYSQL_BACKUP_DIR/$OF.gz
+cp $MYSQL_BACKUP_DIR/$OF.gz $LATEST
