8596eba98b0e24ea7993f1794ee95f642bd29266
Shanan Peters <peters@geology.wisc.edu>
2006-07-02 09:01:08 +0000

    Various updates

--- system/mac/backup_scripts/archive-web.sh
+++ system/mac/backup_scripts/archive-web.sh
@@ -22,7 +22,7 @@ done
 
 # MAKE ARCHIVE OF CGI-BIN
 if [ -x /Volumes/pbdb_RAID/httpdocs/cgi-bin ]; then
-	cp -p -r /Volumes/pbdb_RAID/httpdocs/cgi-bin .
+	cp -P -p -R /Volumes/pbdb_RAID/httpdocs/cgi-bin .
 	
 	# remove static stuff...
 	rm cgi-bin/data/noaa.*
@@ -36,18 +36,32 @@ fi
 
 # MAKE ARCHIVE OF HTML
 if [ -x /Volumes/pbdb_RAID/httpdocs/html ]; then
-	cp -p -r /Volumes/pbdb_RAID/httpdocs/html .
+	cp -P -p -R /Volumes/pbdb_RAID/httpdocs/html .
 		
 	# remove old user generated download files...
 	rm -R html/public/data/*/*.txt
 	rm -R html/public/data/*/*.csv
 	rm -R html/public/data/*/*.tab
-	rm -R html/public/maps/*.*
+	rm -R html/public/data/*/*.conjunct
+	rm -R html/public/maps/*.ai
+	rm -R html/public/maps/*.jpg
+	rm -R html/public/maps/*.png
+	rm -R html/public/maps/*.pict
+	rm -R html/public/maps/*.html
+	rm -R html/public/data/*.csv
+	rm -R html/public/data/*.refs
+	rm -R html/public/confidence/*.jpg
+	rm -R html/public/confidence/*.ai
+	rm -R html/public/confidence/*.png
+	rm -R html/admin/logs/*/*.png
+	rm -R html/admin/logs/*/*.html
+	rm -R html/admin/logs/*/dns_cache.db
 	rm -R html/nam
 	rm html/paleodb/data/*.tab
- #       rm html/paleodb/data/*.txt
-        rm html/paleodb/data/*.csv
-	# rm -R html/paleodb/data
+   	rm html/paleodb/data/*.csv
+  	rm html/paleodb/data/*.conjunct
+ #      rm html/paleodb/data/*.txt
+	rm -R html/paleodb/data
 
 	# make archive
 	tar -cf html.tar html
@@ -56,7 +70,7 @@ fi
 
 # MAKE ARCHIVE OF PALEOSOURCE
 if [ -x /Volumes/pbdb_RAID/httpdocs/paleosource ]; then
-	cp -p -r /Volumes/pbdb_RAID/httpdocs/paleosource .
+	cp -P -p -R /Volumes/pbdb_RAID/httpdocs/paleosource .
 	tar -cf paleosource.tar paleosource
 	gzip paleosource.tar
 fi
