e780ab0fb295778c55639b08abbeeb2ecbaa6a32
Shanan Peters <peters@geology.wisc.edu>
2006-07-02 09:03:02 +0000

    Slight changes

--- system/mac/backup_scripts/send2jcu.sh
+++ system/mac/backup_scripts/send2jcu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 DATE=`/bin/date +%Y%m%d`
-scp -p /Volumes/pbdb_RAID/dailybackups/mysql-backup-${DATE}.gz backup@sifr.jcu.edu.au:~/data/
+scp -p /Volumes/pbdb_RAID/dailybackups/db/mysql-backup-${DATE}.gz backup@sifr.jcu.edu.au:~/data/
 
 scp -p /Users/backup/web/*.tar.gz backup@sifr.jcu.edu.au:~/web
--- system/mac/backup_scripts/send2paleobackup.sh
+++ system/mac/backup_scripts/send2paleobackup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 DATE=`/bin/date +%Y%m%d`
-scp /Volumes/pbdb_RAID/dailybackups/mysql-backup-${DATE}.gz backup@paleobackup.nceas.ucsb.edu:backup.gz
+scp "/Volumes/pbdb_RAID/dailybackups/db/mysql-backup-${DATE}.gz" backup@paleobackup.nceas.ucsb.edu:./
