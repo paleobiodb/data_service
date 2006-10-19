25325fcaa78a56d08e3df47bf029f3d365e21b75
Shanan Peters <peters@geology.wisc.edu>
2006-10-19 04:48:28 +0000

    Improved autosetting script

--- scripts/setOccurrenceTaxonNo.pl
+++ scripts/setOccurrenceTaxonNo.pl
@@ -1,17 +1,14 @@
 #!/usr/bin/perl
 
 use lib '../cgi-bin';
-use DBI;
 use DBConnection;
 use DBTransactionManager;
-use Session;
 use Data::Dumper;
-use TaxonInfo;
+use Class::Date qw(date localdate gmdate now);
+use Taxon;
 
-my $s = Session->new();
 my $dbh = DBConnection::connect();
-my $dbt = DBTransactionManager->new($dbh, $s);
-
+my $dbt = DBTransactionManager->new($dbh);
 
 $doUpdates = 0;
 if ($ARGV[0] eq '--do_sql') {
@@ -31,18 +28,80 @@ if ($ARGV[0] eq '--do_sql') {
 foreach my $table ('reidentifications','occurrences') {
     $pkey = ($table eq 'occurrences') ? 'occurrence_no' : 'reid_no';
     
-    $sql = "select $pkey, a.taxon_name,a.taxon_no,t.taxon_no t_taxon_no from $table t, authorities a where concat(t.genus_name,' ',t.species_name) = a.taxon_name and a.taxon_rank like 'species' and t.taxon_no != a.taxon_no group by t.$pkey";
+    $sql = "select t.$pkey, date_format(t.created,'%m/%d/%Y') tc, date_format(t.modified,'%m/%d/%Y') tm,date_format(a.created,'%m/%d/%Y') ac, date_format(a.modified,'%m/%d/%Y') am,t.created t_created, t.modified t_modified,a.created a_created,t.genus_reso,t.genus_name,t.subgenus_reso,t.subgenus_name,t.species_reso,t.species_name,a.taxon_name,t.taxon_no from $table t LEFT JOIN authorities a ON a.taxon_no=t.taxon_no WHERE genus_name NOT LIKE \"%'%\" and species_name NOT LIKE \"%'%\"";
+    print $sql,"\n";
 
     $sth = $dbh->prepare($sql);
     $sth->execute();
     while($row = $sth->fetchrow_hashref()) {
-        @nos = TaxonInfo::getTaxonNos($dbt,$row->{'taxon_name'});
-        if (scalar(@nos) > 1) {
-            print "WARNING: skipping, multiple possible authorities for $pkey=$row->{$pkey} : $row->{taxon_name}\n";
-        } else {
-            $sql = "UPDATE $table SET modified=modified,taxon_no=".$row->{taxon_no}." WHERE $pkey=$row->{$pkey}";
-            print "$sql\n";
-            $dbh->do($sql)  if ($doUpdates);
+        if ($row->{'genus_name'} =~ /'/) {
+            print "$pkey:$row->{$pkey}: skipping, quote in name $row->{genus_name}";
+        }
+        if ($row->{'genus_reso'} =~ /informal/) {
+            next;
+        }
+        my $name = $row->{'genus_name'};
+        if ($row->{'subgenus_name'}) {
+            $name .= " (".$row->{'subgenus_name'}.")";
+        }
+        if ($row->{'species_name'} !~ /^sp\.|^indet\.|^sp$|^indet$/ && $row->{'species_reso'} !~ 'informal') {
+            $name .= " $row->{'species_name'}";
+        }
+        next if ($row->{'taxon_name'} && $name eq $row->{'taxon_name'});
+        @matches = Taxon::getBestClassification($dbt,$row);
+        $best_no = Taxon::getBestClassification($dbt,$row);
+        if (@matches) {
+            if (@matches && !$best_no) {
+#                print "Skipping, appears to be a homonym issue for $matches[0]->{taxon_name}. ".join(",",map {$_->{'taxon_no'}} @matches)."\n";
+            } else {
+                $best_name = $matches[0]->{'taxon_name'};
+                if ($best_no && $row->{'taxon_no'} != $best_no) {
+                    my $t = TaxonInfo::getTaxa($dbt,{ taxon_no=>$best_no},['taxon_no','taxon_name','taxon_rank','comments','created']);
+                    my $taxon_no = $row->{'taxon_no'};
+                    my $modified = new Class::Date($row->{"t_modified"});
+                    my $created = new Class::Date($row->{"t_created"});
+                    my $t_created = new Class::Date($t->{'created'});
+                    my $cutoff1 = new Class::Date("2004-01-01 00:00:00");
+                    my $cutoff2 = new Class::Date("2006-01-01 00:00:00");
+                    my $rank_change = (@matches > 1 && $matches[0]->{'taxon_name'} eq $matches[1]->{'taxon_name'}) ? 1 : 0;
+                    my $old_match = 0;
+                    my ($t_genus,$t_subgenus,$t_species,$t_subspecies) = Taxon::splitTaxon($t->{taxon_name});
+                    if ($t_created < $cutoff2) {
+                        if ($t->{'taxon_rank'} eq 'genus') {
+                            if ($row->{'genus_name'} eq $t_genus) {
+                                $old_match = 1;
+                            }
+                        } elsif ($t->{'taxon_rank'} =~ /species/) {
+                            if ($row->{'genus_name'} eq $t_genus &&
+                                $row->{'species_name'} eq $t_species) {
+                                $old_match = 1;
+                            }
+                        }
+                    }
+
+                    print "Occ: created $row->{tc} modified $row->{tm} Authority: created $row->{ac} modified $row->{am}\n";
+                    print "$pkey:$row->{$pkey} has taxon ($row->{taxon_no}:$row->{taxon_name}) but should be ($best_no:$best_name)\n";
+                    if (!$taxon_no && !$rank_change && $old_match && $created != $modified && $modified > $cutoff1) {
+                        print "Skipping, taxon_no appears to have been manually set to 0 via reclassification\n";
+                        next;
+                    }
+                    if (!$taxon_no && !$rank_change && $old_match && $row->{'tc'} eq $row->{'tm'} && $created < $cutoff1) {
+                        print "Skipping, taxon_no appears to have been manually set to 0 by alroy\n";
+                        next;
+                    }
+                    if ($t->{'comments'} =~ /sepkoski/i && !$row->{'taxon_no'} && $old_match) {
+                        print "Skipping, sepkoski compendium but appears to have been manually set to 0\n";
+                        next;
+                    }
+                    
+                    $sql = "UPDATE $table SET modified=modified,taxon_no=$best_no WHERE $pkey=$row->{$pkey}";
+                    print "$sql\n";
+                    print "\n";
+                    if ($doUpdates) {
+                        $dbh->do($sql);
+                    }
+                }
+            }
         }
     }
 }
