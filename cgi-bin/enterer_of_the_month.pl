b55a07c08e4b1ced8ea2c34cecf827bdd339a31d
John Alroy <john.alroy@mq.edu.au>
2007-02-03 18:47:05 +0000

    prints table totals

--- cgi-bin/enterer_of_the_month.pl
+++ cgi-bin/enterer_of_the_month.pl
@@ -24,13 +24,17 @@ if ( $mm == 1 )	{
 my $thismonth = $yyyy . $mm . "01000000";
 
 my %total;
+print "\n";
 for my $t ( @tables )	{
 	my $sql = "SELECT first_name,last_name,count(*) AS c FROM person,$t WHERE person_no=enterer_no AND $t.created>$lastmonth AND $t.created<$thismonth GROUP BY enterer_no";
 
 	my @countrefs = @{$dbt->getData($sql)};
+	my $grandtotal = 0;
 	for $c ( @countrefs )	{
 		$total{$c->{first_name}." ".$c->{last_name}} += $c->{c};
+		$grandtotal += $c->{c};
 	}
+	print "$grandtotal $t\n";
 }
 
 my @names = keys %total;
