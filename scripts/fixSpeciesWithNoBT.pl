a66a60eab4bc5766ef16e18d021bcaf8cdaef70a
Shanan Peters <peters@geology.wisc.edu>
2007-06-05 01:27:16 +0000

    moving stuff around

--- scripts/fixSpeciesWithNoBT.pl
+++ scripts/fixSpeciesWithNoBT.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/local/bin/perl
 
 #
 # Fixes weird cases where you have an species, and its genus in the DB,
@@ -8,23 +8,18 @@
 
 use lib '../cgi-bin';
 use DBI;
+use DBConnection;
 use DBTransactionManager;
 use Session;
 use Data::Dumper;
 use TimeLookup;
 use TaxonInfo;
 
-$driver =       "mysql";
-$host =         "localhost";
-$user =         "pbdbuser";
-$db =           "pbdb";
+use strict;
+my $dbh = DBConnection::connect();
+my $dbt = DBTransactionManager->new($dbh);
 
-open PASSWD,"</home/paleodbpasswd/passwd";
-$password = <PASSWD>;
-$password =~ s/\n//;
-close PASSWD;
-
-$doUpdates = 0;
+my $doUpdates = 0;
 if ($ARGV[0] eq '--do_sql') {
     $doUpdates = 1;
     print "RUNNING SQL\n";
@@ -33,17 +28,12 @@ if ($ARGV[0] eq '--do_sql') {
 }
 
 
-my $dbh = DBI->connect("DBI:$driver:database=$db;host=$host", $user, $password, {RaiseError => 1});
-
-# Make a Global Transaction Manager object
-my $s = Session->new();
-my $dbt = DBTransactionManager->new($dbh, $s);
 
 #
 # This script fixes species that are missing the opinion: Genus_a species_a 'belongs to' Genus_a
 #
 
-%ambig = ('Conolophus'=>54363,
+my %ambig = ('Conolophus'=>54363,
           'Eutemnodus'=>53770,
           'Rhectomyax'=>55265,
           'Paliurus'=>55524,
@@ -61,29 +51,28 @@ my $dbt = DBTransactionManager->new($dbh, $s);
           'Convexastrea'=>54146);
 
 
-@ok = ();
-@zero = ();
-@many = ();
+my $sql = "SELECT *,DATE_FORMAT(created,'%Y%m') ym,UNIX_TIMESTAMP(created) ut FROM authorities WHERE taxon_rank IN('subspecies','species','subgenus') AND taxon_name NOT LIKE 'ERROR' AND taxon_name NOT LIKE 'DUPLICATE'";
 
-$sql = "SELECT *,DATE_FORMAT(created,'%Y%m') ym,UNIX_TIMESTAMP(created) ut FROM authorities WHERE taxon_rank IN('subspecies','species') AND taxon_name NOT LIKE 'ERROR' AND taxon_name NOT LIKE 'DUPLICATE'";
+my %by_auth = ();
+my %by_ent = ();
+my %by_mon = ();
 
-%by_auth = ();
-%by_mon = ();
+my %skipped = ();
+my %processed = ();
 
-%skipped = ();
+my @rs = @{$dbt->getData($sql)};
 
-@rs = @{$dbt->getData($sql)};
 foreach my $taxon (@rs) {
-
-    @bits = split(/\s+/,$taxon->{'taxon_name'});
+    my @bits = split(/\s+/,$taxon->{'taxon_name'});
     pop @bits;
-    $higher_name = join(" ",@bits);
+    my $higher_name = join(" ",@bits);
     $sql = "(SELECT o.* FROM opinions o WHERE o.child_spelling_no=$taxon->{taxon_no})";
     $sql .= " UNION ";
     $sql .= "(SELECT o.* FROM opinions o WHERE o.child_no=$taxon->{taxon_no})";
-    @rs2 = @{$dbt->getData($sql)};
-    $found = 0;
-    foreach $row (@rs2) {
+    my @rs2 = @{$dbt->getData($sql)};
+    my $found = 0;
+    foreach my $row (@rs2) {
+        my ($parent_name1,$parent_name2);
         if ($row->{'parent_no'}) {
             $parent_name1 = ${$dbt->getData("SELECT taxon_name FROM authorities WHERE taxon_no=$row->{parent_no}")}[0]->{'taxon_name'};
             $parent_name2 = ${$dbt->getData("SELECT taxon_name FROM authorities WHERE taxon_no=$row->{parent_spelling_no}")}[0]->{'taxon_name'};
@@ -97,32 +86,34 @@ foreach my $taxon (@rs) {
     }
 
     if (!$found) {
-
+        print "\n";
         if ($taxon->{comments} =~ /kosnik taxon/) {
             print "Kosnik taxon\n";
         }
 
-        if ($taxon->{ym} =~ /2005/) {
-            print Dumper($taxon);
-        }
-        print "\nCould not find $taxon->{taxon_no} $taxon->{taxon_name} belongs to $higher_name -- ref is authority = '$taxon->{ref_is_authority}' -- ref is $taxon->{reference_no} ";
-        $ref1 = Reference->new($dbt,$taxon->{reference_no});
+        print "Could not find $taxon->{taxon_no} $taxon->{taxon_name} belongs to $higher_name -- ref is authority = '$taxon->{ref_is_authority}' -- ref is $taxon->{reference_no}";
+        my $ref1 = Reference->new($dbt,$taxon->{reference_no});
         if ($ref1) {
             print $ref1->authors();
-        } 
+        } else {
+            print " ERROR: NO REFERENCE";
+        }
         print "\n";
+
         if (@rs2) {
-            foreach $row (@rs2) {
+            foreach my $row (@rs2) {
                 print "    Found this opinion: $row->{child_no} ($row->{child_spelling_no}) $row->{status} $row->{parent_no} $row->{parent_name} ($row->{parent_spelling_no})\n";
             }
         } else {
-
-            $sqlt = "SELECT * FROM opinions WHERE enterer_no=$taxon->{enterer_no} AND (UNIX_TIMESTAMP(created) < ".($taxon->{ut}+3)." AND UNIX_TIMESTAMP(created) > ".($taxon->{ut}-3).") LIMIT 3";
-            @rst = @{$dbt->getData($sqlt)};
+            my $sqlt = "SELECT * FROM opinions WHERE enterer_no=$taxon->{enterer_no} AND (UNIX_TIMESTAMP(created) <= ".($taxon->{ut}+2)." AND UNIX_TIMESTAMP(created) >= ".($taxon->{ut}-2).") LIMIT 3";
+            my @rst = @{$dbt->getData($sqlt)};
             if (@rst) {
-                print "SKIPPING $taxon->{taxon_no}: found opinions created by $taxon->{enterer_no} within same 3 seconds:\n";
+                # We skip these because they're usually just typos that happened on authority autocreation - i.e.
+                # Plesiocetopsis burtinii was autocreated but later corrected to Pleiocetus burtinii, leaving the
+                # original stranded
+                print "ERROR: SKIPPING $taxon->{taxon_no}: found opinions created by $taxon->{enterer_no} within same 2 seconds:\n";
                 $skipped{$taxon->{taxon_no}} = 1;
-                foreach $row (@rst) {
+                foreach my $row (@rst) {
                     print "    Found this opinion: $row->{child_no} ($row->{child_spelling_no}) $row->{status} $row->{parent_no} ($row->{parent_spelling_no})\n";
                 }
                 next;
@@ -130,37 +121,56 @@ foreach my $taxon (@rs) {
             $by_auth{$taxon->{authorizer_no}}++;
             $by_ent{$taxon->{enterer_no}}++;
             $by_mon{$taxon->{ym}}++;
-            $taxon_short = $taxon->{taxon_name};
+            my $taxon_short = $taxon->{taxon_name};
             chop $taxon_short; chop $taxon_short;
-            $sql1 = "SELECT * FROM authorities WHERE taxon_name LIKE '".$taxon_short."%'";
-            @r1 = @{$dbt->getData($sql1)};
+            my $sql1 = "SELECT * FROM authorities WHERE taxon_name LIKE '".$taxon_short."%'";
+            my @r1 = @{$dbt->getData($sql1)};
             if (scalar(@r1) > 1) {
-                print "Skipping $taxon->{taxon_name}, too similar to ".join (', ',map {"$_->{taxon_no} $_->{taxon_name}"} @r1)."\n";
+                print "ERROR: Skipping $taxon->{taxon_name}, too similar to ".join (', ',map {"$_->{taxon_no} $_->{taxon_name}"} @r1)."\n";
+                $skipped{$taxon->{taxon_no}} = 1;
+                next;
             }
-            @parents = TaxonInfo::getTaxon($dbt,'taxon_name'=>$higher_name); 
-            $tt = 0;
+            my @parents = TaxonInfo::getTaxa($dbt,{'taxon_name'=>$higher_name}); 
+            my $tt = 0;
             if ($taxon->{authorizer_no} == 4) {
                 #print "Alroy $taxon->{taxon_name} missing bt to $higher_name\n";
             }
-            for $p (@parents) {
+            foreach my $p (@parents) {
                 if ($p->{'type_taxon_no'} && $p->{'type_taxon_no'} == $taxon->{'taxon_no'}){
                     $tt = $p->{'taxon_no'};
                     print "FOUND type_taxon linking $p->{taxon_no} to $p->{type_taxon_no}\n";
                 }
             }
-            if (scalar(@parents) == 1 || $ambig{$higher_name} || $tt) { 
+            if (!@parents) {
+                my ($g,$sg,$sp,$ssp) = Taxon::splitTaxon($higher_name);
+                if ($sg) {
+                    my $taxon = TaxonInfo::getTaxa($dbt,{taxon_name=>$sg});
+                    if ($taxon) {
+                        print "FOUND $taxon->{taxon_no}:$taxon->{taxon_name}\n";
+                    }
+                }
+            }
+            
+            if (scalar(@parents) == 1 || $ambig{$higher_name} || $tt) {
+                my $parent_no;
                 if ($ambig{$higher_name}) {
                     $parent_no = $ambig{$higher_name};
                 } elsif ($tt) {
                     $parent_no = $tt;
                 } else {
-                    $parent_no=$parents[0]->{'taxon_no'};
+                    $parent_no= $parents[0]->{'taxon_no'};
                 }
                 if ($parents[0]) {
-                    $gp = TaxonInfo::getMostRecentParentOpinion($dbt,$parents[0]->{taxon_no});
-                    if ($gp->{status} =~ /synonym|replaced|homonym/) {
-                        print "Parent is $gp->{status} of $gp->{parent_no}, not adding BT\n";
+                    my $gp = TaxonInfo::getMostRecentClassification($dbt,$parents[0]->{taxon_no});
+                    if ($gp->{status} =~ /synonym|replaced|homonym|subgroup/) {
+                        print "ERROR: Parent is $gp->{status} of $gp->{parent_no}, not adding BT\n";
+                        $skipped{$taxon->{taxon_no}} = 1;
+                        next;
                     }
+                }
+                if ($taxon->{ym} =~ /2005|2006/) {
+                    print "Skipping, from 2005 or 20006\n";
+                    next;
                 }
                 if ($taxon->{'ref_is_authority'} !~ /YES/i) {
                     if (!$taxon->{'pubyr'}) {
@@ -174,55 +184,79 @@ foreach my $taxon (@rs) {
                             if ($parents[0]->{ref_is_authority} =~ /YES/) {
                                 print "Found parent from same ref with ref_is_authority YES\n"; 
                                 $sql = "UPDATE authorities SET ref_is_authority='YES' WHERE taxon_no=$taxon->{taxon_no}";
-                                #print $sql."\n";
-                                #$dbh->do($sql) if ($doUpdates); # lay off for now
-                                $sql = "INSERT INTO opinions (authorizer_no,enterer_no,reference_no,child_no,child_spelling_no,status,parent_no,parent_spelling_no,created,ref_has_opinion,author1init,author1last,author2init,author2last,otherauthors,pubyr,pages,figures) VALUES ($taxon->{authorizer_no},$taxon->{enterer_no},$taxon->{reference_no},$taxon->{taxon_no},$taxon->{taxon_no},'belongs to',$parent_no,$parent_no,NOW(),'YES','','','','','','','$taxon->{pages}','$taxon->{figures}')";
                                 print $sql."\n";
-                                $dbh->do($sql) if ($doUpdates);
+                                if ($doUpdates) {
+                                    $dbh->do($sql);
+                                }
+                                $taxon->{ref_is_authority} = 'YES';
+                                addOpinion($dbt,$taxon,$parent_no);
                                 next;
                             } elsif ($parents[0]->{type_taxon_no} == $taxon->{taxon_no}) {
                                 print "Found parent from same ref with ref_is_authority NO but linked by type_taxon_no\n"; 
-                                # Don't use parnet for authority data - not reliable i.e. Polacantus: Owen 1865, Polacanthus foxii, type taxon: Hulke 1881
-                                #if ($parents[0]->{author1last} && !$taxon->{author1last}) {
-                                #    print "Using parent for authority data\n";
-                                #    $sql = "INSERT INTO opinions (authorizer_no,enterer_no,reference_no,child_no,child_spelling_no,status,parent_no,parent_spelling_no,created,ref_has_opinion,author1init,author1last,author2init,author2last,otherauthors,pubyr,pages,figures) VALUES ($taxon->{authorizer_no},$taxon->{enterer_no},$taxon->{reference_no},$taxon->{taxon_no},$taxon->{taxon_no},'belongs to',$parent_no,$parent_no,NOW(),'$parents[0]->{ref_is_authority}',".$dbh->quote($parents[0]->{author1init}).",".$dbh->quote($parents[0]->{author1last}).",".$dbh->quote($parents[0]->{author2init}).",".$dbh->quote($parents[0]->{author2last}).",".$dbh->quote($parents[0]->{otherauthors}).",'$parents[0]->{pubyr}','$taxon->{pages}','$taxon->{figures}')";
-                                #} else {
-                                    $sql = "INSERT INTO opinions (authorizer_no,enterer_no,reference_no,child_no,child_spelling_no,status,parent_no,parent_spelling_no,created,ref_has_opinion,author1init,author1last,author2init,author2last,otherauthors,pubyr,pages,figures) VALUES ($taxon->{authorizer_no},$taxon->{enterer_no},$taxon->{reference_no},$taxon->{taxon_no},$taxon->{taxon_no},'belongs to',$parent_no,$parent_no,NOW(),'$taxon->{ref_is_authority}',".$dbh->quote($taxon->{author1init}).",".$dbh->quote($taxon->{author1last}).",".$dbh->quote($taxon->{author2init}).",".$dbh->quote($taxon->{author2last}).",".$dbh->quote($taxon->{otherauthors}).",'$taxon->{pubyr}','$taxon->{pages}','$taxon->{figures}')";
-                                #}
-                                print $sql."\n";
-                                $dbh->do($sql) if ($doUpdates);
+                                addOpinion($dbt,$taxon,$parent_no);
                                 next;
                             }
                         }
                     }
                     # At this point, screwed cause we have the opinion data will be incomplete (no pubyr, author1last), do it anyways tho, better than nothing
-                    $sql = "INSERT INTO opinions (authorizer_no,enterer_no,reference_no,child_no,child_spelling_no,status,parent_no,parent_spelling_no,created,ref_has_opinion,author1init,author1last,author2init,author2last,otherauthors,pubyr,pages,figures) VALUES ($taxon->{authorizer_no},$taxon->{enterer_no},$taxon->{reference_no},$taxon->{taxon_no},$taxon->{taxon_no},'belongs to',$parent_no,$parent_no,NOW(),'$taxon->{ref_is_authority}',".$dbh->quote($taxon->{author1init}).",".$dbh->quote($taxon->{author1last}).",".$dbh->quote($taxon->{author2init}).",".$dbh->quote($taxon->{author2last}).",".$dbh->quote($taxon->{otherauthors}).",'$taxon->{pubyr}','$taxon->{pages}','$taxon->{figures}')";
-                    print $sql."\n";
-                    $dbh->do($sql) if ($doUpdates);
+                    addOpinion($dbt,$taxon,$parent_no);
                     next;
                 } else {
                     if (!$taxon->{'reference_no'}) {
                         print "ERROR, no reference_no\n";
+                        $skipped{$taxon->{taxon_no}} = 1;
                         next;
                     } 
-                    $sql = "INSERT INTO opinions (authorizer_no,enterer_no,reference_no,child_no,child_spelling_no,status,parent_no,parent_spelling_no,created,ref_has_opinion,author1init,author1last,author2init,author2last,otherauthors,pubyr,pages,figures) VALUES ($taxon->{authorizer_no},$taxon->{enterer_no},$taxon->{reference_no},$taxon->{taxon_no},$taxon->{taxon_no},'belongs to',$parent_no,$parent_no,NOW(),'YES','','','','','','','$taxon->{pages}','$taxon->{figures}')";
-                    print $sql."\n";
-                    $dbh->do($sql) if ($doUpdates);
+                    addOpinion($dbt,$taxon,$parent_no);
                     next;
                 }
             } elsif (scalar(@parents) == 0) {
                 print "    ERROR, could not find $higher_name in DB\n";
+                $skipped{$taxon->{taxon_no}} = 1;
+                next;
             } else {
                 print "    ERROR, ambiguous parent $higher_name\n";
+                $skipped{$taxon->{taxon_no}} = 1;
+                next;
             }
         }
     }
 }
-@srt = sort {$a <=> $b} keys %by_mon;
+my @srt = sort {$a <=> $b} keys %by_mon;
 print "BY AUTH:\n".Dumper(\%by_auth)."\n";
 print "BY ENT:\n".Dumper(\%by_ent)."\n";
 print "BY MONTH:\n";
 print "$_ => $by_mon{$_}\n" for (@srt);
 
 print "";
-print Dumper(\%skipped);
+print "SKIPPED: ".join(", ",sort {$a <=> $b} keys %skipped)."\n";
+print "PROCESSED: ".join(", ",sort {$a <=> $b} keys %processed)."\n";
+
+
+
+sub addOpinion {
+    my ($dbt,$taxon,$parent_no)  = @_;
+    my $dbh = $dbt->dbh;
+
+    my ($a1i,$a1,$a2i,$a2,$other,$pubyr,$is_authority);
+    if ($taxon->{'ref_is_authority'} =~ /yes/i) {
+        $a1i = "''"; $a1 = "''"; $a2i = "''"; $a2 = "''"; $other = "''"; $pubyr="''"; $is_authority="'YES'";
+    } else {
+        $a1i = $dbh->quote($taxon->{'author1init'});
+        $a1 = $dbh->quote($taxon->{'author1last'});
+        $a2i = $dbh->quote($taxon->{'author2init'});
+        $a2 = $dbh->quote($taxon->{'author2last'});
+        $other = $dbh->quote($taxon->{'author1init'});
+        $is_authority="''";
+    }
+    my $pages = $dbh->quote($taxon->{'pages'});
+    my $figures= $dbh->quote($taxon->{'figures'});
+
+    print "ADDING OPINION FOR $taxon->{taxon_name} AUTHORIZER $taxon->{authorizer_no}\n";
+    $sql = "INSERT INTO opinions (authorizer_no,enterer_no,reference_no,child_no,child_spelling_no,status,spelling_reason,parent_no,parent_spelling_no,created,ref_has_opinion,author1init,author1last,author2init,author2last,otherauthors,pubyr,pages,figures) VALUES ($taxon->{authorizer_no},$taxon->{enterer_no},$taxon->{reference_no},$taxon->{taxon_no},$taxon->{taxon_no},'belongs to','original spelling',$parent_no,$parent_no,NOW(),$is_authority,$a1i,$a1,$a2i,$a2,$other,$pubyr,$pages,$figures)";
+    print $sql."\n";
+    if ($doUpdates) {
+        $dbh->do($sql);
+    }
+    $processed{$taxon->{taxon_no}} = 1;
+}
--- scripts/tc_display.pl
+++ scripts/tc_display.pl
@@ -5,70 +5,97 @@ use DBConnection;
 use DBTransactionManager;
 use TaxaCache;
 use Data::Dumper;
+use Classification;
 use TaxonInfo;
 
 my $dbh = DBConnection::connect();
 my $dbt = new DBTransactionManager($dbh);
 
-my ($compare,$list,$taxon);
+my ($compare,$list,$taxon,@exclude);
+my @taxa = ();
+my $taxon = "";
 foreach (@ARGV) {
-    if ($_ =~ /-l/) { # --list
+    if ($_ =~ /-e(xclude)*([A-Za-z]+)/) { # --list
+        push @exclude,$2,
+    } elsif ($_ =~ /-l/) { # --list
         $list = 1;
     } elsif ($_ =~ /-d(epth)*([0-9]+)/) { # --depth
         $depth = $2;
     } elsif ($_ =~ /-c/) { # --compare
         $compare = 1;
     } else {
-        push @REST,$_;
+        push @taxa, $_;
     }
 }
 
+foreach my $e (@exclude) {
+    my @x = TaxonInfo::getTaxa($dbt,{'taxon_name'=>$e});
+    $e = $x[0]->{'taxon_no'};
+}
 
-$taxon = $REST[0];
-@t = TaxonInfo::getTaxon($dbt,{'taxon_name'=>$taxon});
-$taxon_no = $t[0]->{'taxon_no'};
+foreach my $t (@taxa) {
+    my @x = TaxonInfo::getTaxa($dbt,{'taxon_name'=>$t});
+    $t = $x[0]->{'taxon_no'};
+}
 
 print ($compare ? "COMPARING" : "SHOWING");
 print " ";
 print ($list ? "LIST" : "TREE");
 print ((!$list && $depth) ? "-DEPTH $depth-.":"");
-print " of $taxon ($taxon_no)\n";
+print " excluding ".join(", ",@exclude) if (@exclude);
+print " of ".join(", ",@taxa)."\n";
 
-if ($taxon_no) {
+foreach $taxon_no (@taxa) {
     if ($list) {
-        @children = TaxaCache::getChildren($dbt,$taxon_no);
+        @children = TaxaCache::getChildren($dbt,$taxon_no,'','',\@exclude);
         print join(", ",sort {$a<=>$b} @children)."\n";
     } else {
-        my $tree = TaxaCache::getChildren($dbt,$taxon_no,'tree',$depth);
-        $tree->{'depth'} = 0;
-        @nodes_to_print = ($tree);
-        while (@nodes_to_print) {
-            my $node = shift @nodes_to_print;
-            print "  " for (0..$node->{'depth'});
-            print $node->{'taxon_name'}."   ";
-            foreach my $row (@{$node->{'spellings'}}) {
-               print " sp. $row->{taxon_name}"; 
-            }
-            foreach my $row (@{$node->{'synonyms'}}) {
-                print " syn. $row->{taxon_name}"; 
-            }
-            print "\n";
-            my @children = ();
-            push @children, @{$node->{'children'}};
-            foreach (@children) {
-                $_->{'depth'} = $node->{'depth'} + 1;
-            }
-            unshift @nodes_to_print,@children;
-        }
+        my $tree = TaxaCache::getChildren($dbt,$taxon_no,'tree','',\@exclude);
+        printTree($tree);
     }
 
     if ($compare) {
         print "vs.\n";
         if ($list) {
-            @c = PBDBUtil::taxonomic_search($dbt,$taxon_no);
+            @c = Classification::taxonomic_search($dbt,$taxon_no);
             print join(", ",sort {$a<=>$b} @c)."\n";
         } else {
-            PBDBUtil::getChildren($dbt,$taxon_no);
+            my $tree = Classification::getChildren($dbt,$taxon_no,'tree');
+            printTree($tree);
         }
+    }
+}
+
+sub printTree {
+    my $tree = shift;
+    $tree->{'depth'} = 0;
+    @nodes_to_print = ($tree);
+    while (@nodes_to_print) {
+        my $node = shift @nodes_to_print;
+        print "  " for (0..$node->{'depth'});
+        if ($node->{synonym}) {
+            print " syn. ";
+        }
+        print $node->{'taxon_name'}."   ";
+        foreach my $row (@{$node->{'spellings'}}) {
+           print " sp. $row->{taxon_name}"; 
+        }
+        #foreach my $row (@{$node->{'synonyms'}}) {
+        #    print " syn. $row->{taxon_name}"; 
+        #}
+        print "\n";
+        my @children = ();
+        push @children, @{$node->{'children'}};
+        foreach (@children) {
+            $_->{'depth'} = $node->{'depth'} + 1;
+        }
+        unshift @nodes_to_print,@children;
+        my @children = ();
+        push @children, @{$node->{'synonyms'}};
+        foreach (@children) {
+            $_->{'depth'} = $node->{'depth'};
+            $_->{'synonym'} = 1;
+        }
+        unshift @nodes_to_print,@children;
     }
 }
