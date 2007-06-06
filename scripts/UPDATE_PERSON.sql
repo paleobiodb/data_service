3440bf9861186e60970ed94694358ef126937b0c
Shanan Peters <peters@geology.wisc.edu>
2007-06-06 20:44:57 +0000

    some more cleanup

--- scripts/UPDATE_PERSON.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `person` ADD `first_name` VARCHAR( 30 ) NOT NULL AFTER `reversed_name` ,
-ADD `last_name` VARCHAR( 30 ) NOT NULL AFTER `first_name` ,
-ADD `homepage` VARCHAR( 80 ) AFTER `email` ;
-
