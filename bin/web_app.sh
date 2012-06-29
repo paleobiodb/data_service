6cbe3bdb943ca07dd68f9a8552924505c8b57d3b
Michael McClennen <mmcclenn@geology.wisc.edu>
2012-06-29 17:50:20 -0500

    added web_app.sh, a wrapper for web_app.pl

---
+++ bin/web_app.sh
@@ -0,0 +1,5 @@
+# This a wrapper for web_app.pl, to set the necessary environment variable
+# so that DBD::mysql.pm can properly connect to the mysqld server.
+#
+export DYLD_LIBRARY_PATH=/usr/local/mysql/lib
+starman --listen :3000 --workers=2 bin/web_app.pl
