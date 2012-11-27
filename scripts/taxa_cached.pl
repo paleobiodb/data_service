e3e32bc30bc9b6fcf94ff4624baa02e9e2bb34e1
Michael McClennen <mmcclenn@paleodb.science.mq.edu.au>
2012-11-28 10:22:40 +1100

    minor updates

--- scripts/taxa_cached.pl
+++ scripts/taxa_cached.pl
@@ -96,8 +96,8 @@ sub doUpdate {
 
 sub daemonize {
     chdir '/'                 or die "Can't chdir to /: $!";
-    open STDOUT, '>>/home/peters/testd.log' or die "Can't write to log: $!";
-    open STDERR, '>>/home/peters/testd_err.log' or die "Can't write to errlog: $!";
+    #open STDOUT, '>>/home/peters/testd.log' or die "Can't write to log: $!";
+    #open STDERR, '>>/home/peters/testd_err.log' or die "Can't write to errlog: $!";
     open STDIN, '/dev/null'   or die "Can't read /dev/null: $!";
 #    open STDOUT, '>>/dev/null' or die "Can't write to /dev/null: $!";
 #    open STDERR, '>>/dev/null' or die "Can't write to /dev/null: $!";
