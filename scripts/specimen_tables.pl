9c629b5a008f9e737ef135e22bb1cbb8249e134d
Michael McClennen <mmcclenn@geology.wisc.edu>
2019-10-15 19:24:04 -0500

    Fix a few details necessary to finish the merge.

--- scripts/specimen_tables.pl
+++ scripts/specimen_tables.pl
@@ -1,24 +1,15 @@
 #!/usr/bin/env perl
 # 
-<<<<<<< HEAD
 # specimen_tables.pl
 # 
 # Establish or reload the specimen tables.
-=======
-# timescale_tables.pl
-# 
-# Establish or reload the timescale tables.
->>>>>>> editing
 
 use strict;
 
 use lib '../lib', 'lib';
 use Getopt::Long;
 use Pod::Usage;
-<<<<<<< HEAD
-=======
 use Try::Tiny;
->>>>>>> editing
 
 # The following modules are all part of the "new" pbdb.
 
@@ -27,25 +18,13 @@ use CoreFunction qw(connectDB
 use ConsoleLog qw(initMessages
 		  logMessage
 		  logTimestamp);
-<<<<<<< HEAD
-use SpecimenTables qw(init_specelt_tables load_specelt_tables build_specelt_map);
-=======
 use SpecimenTables qw(establish_spec_element_tables establish_extra_specimen_tables
 		      load_spec_element_tables);
->>>>>>> editing
 
 
 # First parse option switches.  If we were given an argument, then use that as
 # the database name overriding what was in the configuration file.
 
-<<<<<<< HEAD
-my ($opt_init_tables, $opt_load_data, $opt_map_elts, $opt_debug,
-    $opt_help, $opt_man, $opt_verbose);
-
-GetOptions("init-elts" => \$opt_init_tables,
-	   "load-elts" => \$opt_load_data,
-	   "map-elts" => \$opt_map_elts,
-=======
 my ($opt_help, $opt_man, $opt_verbose, $opt_debug);
 my ($opt_elt_tables, $opt_spec_tables, $opt_elt_data, $opt_place_data);
 
@@ -55,7 +34,6 @@ GetOptions("init-elt-tables" => \$opt_elt_tables,
 	   "init-spec-tables" => \$opt_spec_tables,
 	   "load-elt-data" => \$opt_elt_data,
 	   "load-place-data=s" => \$opt_place_data,
->>>>>>> editing
 	   "debug" => \$opt_debug,
 	   "help|h" => \$opt_help,
 	   "man" => \$opt_man,
@@ -66,11 +44,7 @@ GetOptions("init-elt-tables" => \$opt_elt_tables,
 # Check for documentation requests
 
 pod2usage(1) if $opt_help;
-<<<<<<< HEAD
-pod2usage(2) unless $opt_init_tables || $opt_load_data || $opt_map_elts;
-=======
 pod2usage(2) unless $opt_elt_tables || $opt_spec_tables || $opt_elt_data || $opt_place_data;
->>>>>>> editing
 pod2usage(-exitval => 0, -verbose => 2) if $opt_man;
 
 # If we get here, then we have stuff to do. So get a database handle.
@@ -83,26 +57,6 @@ logTimestamp();
 
 # Then process the options and execute the necessary functions.
 
-<<<<<<< HEAD
-my $options = { };
-
-$options->{verbose} = $opt_verbose ? 3 : 2;
-$options->{debug} = 1 if $opt_debug;
-
-if ( $opt_init_tables )
-{
-    init_specelt_tables($dbh, $options);
-}
-
-if ( $opt_load_data )
-{
-    load_specelt_tables($dbh, '-', $options);
-}
-
-if ( $opt_map_elts )
-{
-    build_specelt_map($dbh, 'taxon_trees', $options);
-=======
 $options->{verbose} = $opt_verbose ? 3 : 2;
 $options->{debug} = 1 if $opt_debug;
 
@@ -131,7 +85,6 @@ if ( $opt_place_data )
 {
     my $filename = shift;
     load_place_data_table($dbh, $opt_place_data, $options);
->>>>>>> editing
 }
 
 1;
