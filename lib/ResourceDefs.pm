1e62a2b1663af784c08d162f88ad9a101ca38f02
Michael McClennen <mmcclenn@geology.wisc.edu>
2024-07-11 20:30:17 -0500

    Changes to the educational resource entry system so that it works properly with the updated EditTransaction system and passes the tests. It now passes all except the final test, which I am punting on for now.

--- lib/ResourceDefs.pm
+++ lib/ResourceDefs.pm
@@ -48,7 +48,7 @@ use TableDefs qw(set_table_name set_table_group set_table_property set_column_pr
     
     set_table_property(RESOURCE_QUEUE => CAN_POST => 'LOGGED_IN');
     set_table_property(RESOURCE_QUEUE => CAN_VIEW => 'LOGGED_IN');
-    set_table_property(RESOURCE_QUEUE => ALLOW_DELETE => 1);
+    # set_table_property(RESOURCE_QUEUE => ALLOW_DELETE => 1);
     set_table_property(RESOURCE_QUEUE => PRIMARY_KEY => "eduresource_no");
     set_table_property(RESOURCE_QUEUE => PRIMARY_FIELD => "eduresource_id");
     
